ab94b867315768f510d785f92f4dc4e7f68fb0ff
JRoLee <cnsfw123@gmail.com>
2026-04-01 00:08:44 +0900

    init : initialize project
    init : initialize project

---
+++ Source/SagoMagic/SampleProject/SagoMagic.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SagoMagic.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SagoMagic, "SagoMagic" );
+
+DEFINE_LOG_CATEGORY(LogSagoMagic)
+ 

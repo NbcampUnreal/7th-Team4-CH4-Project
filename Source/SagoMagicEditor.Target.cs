964cd85ac4782e983bc870e9b2dcea5ebf54e2d6
JRoLee <cnsfw123@gmail.com>
2026-04-01 01:10:04 +0900

    add : GIt status branch module
    add : GIt status branch module

--- Source/SagoMagicEditor.Target.cs
+++ Source/SagoMagicEditor.Target.cs
@@ -11,5 +11,11 @@ public class SagoMagicEditorTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("SagoMagic");
-	}
+        RegisterModulesCreatedByRider();
+    }
+
+    private void RegisterModulesCreatedByRider()
+    {
+        ExtraModuleNames.AddRange(new string[] { "GitStatusBranch" });
+    }
 }

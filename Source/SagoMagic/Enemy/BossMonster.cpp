bc9368f4ff37cc847fcdfeb030892d64a3b1cdde
limyt0 <dudx45@gmail.com>
2026-04-01 16:17:09 +0900

    add: MonsterBase class

---
+++ Source/SagoMagic/Enemy/BossMonster.cpp
@@ -0,0 +1,2 @@
+﻿#include "Enemy/BossMonster.h"
+
---
+++ Source/SagoMagic/Enemy/BossMonster.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/MonsterBase.h"
+#include "BossMonster.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API ABossMonster : public AMonsterBase
+{
+	GENERATED_BODY()
+	
+};

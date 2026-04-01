143b536756bee7a878e100ec0c1850de7317d0fe
JRoLee <cnsfw123@gmail.com>
2026-04-01 13:09:28 +0900

    add : GameplayTag macro to project
    add : GameplayTag macro to project

---
+++ Source/SagoMagic/GameplayTags/Character/SMCharacterTag.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace SMCharacterTag
+{
+    /**
+    *사용 양식
+    *UE_DECLARE_GAMEPLAY_TAG_EXTERN(간편하게 사용할 태그이름);
+    *예시
+    *UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Reload_Ammo);
+    */
+}
---
+++ Source/SagoMagic/GameplayTags/Character/SMSkillTag.cpp
@@ -0,0 +1,11 @@
+#include "GameplayTags/Character/SMSkillTag.h"
+
+namespace SMSkillTag
+{
+    /**
+     *사용 양식
+     *UE_DEFINE_GAMEPLAY_TAG(간편하게 사용할 태그이름, 해당 이름과 연동될 실제 GameplayTag);
+     *예시
+     *UE_DEFINE_GAMEPLAY_TAG(Data_Reload_Ammo, "Data.Reload.Ammo");
+     */
+}

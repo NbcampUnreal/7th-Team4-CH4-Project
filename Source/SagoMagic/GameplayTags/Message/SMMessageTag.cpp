d59e3aafd66b37256723b3c80fc6c52ccae1b92b
Cheolmin Joo <cjfals4539@gmail.com>
2026-04-02 02:05:02 +0900

    add: MessageTag Initialized
    MessageTag 초기 설정

---
+++ Source/SagoMagic/GameplayTags/Message/SMMessageTag.cpp
@@ -0,0 +1,20 @@
+#include "GameplayTags/Message/SMMessageTag.h"
+
+namespace SMMessageTag
+{
+    /**
+     *사용 양식
+     *UE_DEFINE_GAMEPLAY_TAG(간편하게 사용할 태그이름, 해당 이름과 연동될 실제 GameplayTag);
+     *예시
+     *UE_DEFINE_GAMEPLAY_TAG(Data_Reload_Ammo, "Data.Reload.Ammo");
+     */
+
+    /** 인벤토리 갱신 메시지 채널 태그 정의 */
+    UE_DEFINE_GAMEPLAY_TAG(Inventory_Updated, "SM.Message.Inventory.Updated");
+
+    /** 스킬 요약 갱신 메시지 채널 태그 정의 */
+    UE_DEFINE_GAMEPLAY_TAG(Inventory_SkillSummaryUpdated, "SM.Message.Inventory.SkillSummaryUpdated");
+
+    /** 퀵슬롯 갱신 메시지 채널 태그 정의 */
+    UE_DEFINE_GAMEPLAY_TAG(Inventory_QuickSlotUpdated, "SM.Message.Inventory.QuickSlotUpdated");
+}

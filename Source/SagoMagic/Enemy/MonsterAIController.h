5371a3d23a92f4e91fd4b9eca55bed4b32df87b4
limyt0 <dudx45@gmail.com>
2026-04-01 16:19:28 +0900

    add: MonsterAIController Class

---
+++ Source/SagoMagic/Enemy/MonsterAIController.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MonsterAIController.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API AMonsterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};

0cc0a1a1dfc9ec09a1055a36403c99bae114b382
허태린 <ouranos0902@gmail.com>
2026-04-01 19:00:17 +0900

    update : skeletal SkillBase Network
    네트워크에 필요한 스켈레탈 코드 작성

--- Source/SagoMagic/GAS/Abilities/UGA_SkillBase.cpp
+++ Source/SagoMagic/GAS/Abilities/UGA_SkillBase.cpp
@@ -1,5 +1,53 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GAS/Abilities/UGA_SkillBase.h"
+#include "AbilitySystemComponent.h"
+
+UGA_SkillBase::UGA_SkillBase()
+{
+    //** 어빌리티 인스턴스 생성 - 단 하나 */
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+    //** 어빌리티가 네트워크 어디서 실행될지 - 클라이언트 */
+    NetExecutionPolicy =EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+    //** 인스턴스를 네트워크로 복제할지 - 복제안함 */
+    ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
+}
+
+bool UGA_SkillBase::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+    return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+}
+
+void UGA_SkillBase::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_SkillBase::ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+    Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+}
+
+/*
+//TODO : ASC 완성 후 후첨
+bool UGA_SkillBase::BatchRPCTryActivateAbility(FGameplayAbilitySpecHandle InAbilityHandle, bool EndAbilityImmediately)
+{
+    USMAbilitySystemComponent* SMASC = Cast<USMAbilitySystemComponent>(GetAbilitySystemComponentFromActorInfo());
+    if (SMASC)
+    {
+        return SMASC->BatchRPCTryActivateAbility(InAbilityHandle, EndAbilityImmediately);
+    }
+    return false;
+}
+*/
+
+FString UGA_SkillBase::GetCurrentPredictionKeyStatus()
+{
+    UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+    return ASC->ScopedPredictionKey.ToString() + " is valid for more prediction: " + (ASC->ScopedPredictionKey.IsValidForMorePrediction() ? TEXT("true") : TEXT("false"));
+}
+
+bool UGA_SkillBase::IsPredictionKeyValidForMorePrediction() const
+{
+    UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+    return ASC->ScopedPredictionKey.IsValidForMorePrediction();
+}
 
--- Source/SagoMagic/GAS/Abilities/UGA_SkillBase.h
+++ Source/SagoMagic/GAS/Abilities/UGA_SkillBase.h
@@ -1,17 +1,66 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Abilities/GameplayAbility.h"
 #include "UGA_SkillBase.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
-class SAGOMAGIC_API UUGA_SkillBase : public UGameplayAbility
+class SAGOMAGIC_API UGA_SkillBase : public UGameplayAbility
 {
 	GENERATED_BODY()
-	
+	UGA_SkillBase();
+
+public:
+
+    //** 어빌리티 인터페이스 */
+
+    virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const override;
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+    virtual void ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+
+
+/*
+    // 어빌리티 Handle을 활성화, 모든 RPC를 하나로 묶으려고 시도
+    // 특정 상황에서만 발생하는 모든 RPC만 묶음, 한 프레임 내에서 처리
+    //ActivateAbility, EndAbility 2개의 RPC 대신 하나의 RPC로 처리 할 수 있음 마취 Bunch TODO로 남겨두지만 추후 사라질 수 있음.
+    UFUNCTION(BlueprintCallable, Category = "SagoMagic|Ability")
+    virtual bool BatchRPCTryActivateAbility(FGameplayAbilitySpecHandle InAbilityHandle, bool EndAbilityImmediately);
+*/
+
+    //** 현재 Prediction Key를 반환하고 더 많은? 예측에 유효한지 Vaild 한다 ,Ability Prediction Window 디버깅에 사용 */
+    UFUNCTION(BlueprintCallable, Category = "SagoMagic|Ability")
+    virtual FString GetCurrentPredictionKeyStatus();
+
+    //* 현재 Prediction Key가 추가 예측에 유효한지 -> Prediction Key가 여전히 사용 가능한지 여부 확인 */
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "SagoMagic|Ability")
+    virtual bool IsPredictionKeyValidForMorePrediction() const;
+
+
+
+protected:
+
+    /**ㅅ,킬효과 서버*/
+    virtual void OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo) {}
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SagoMagic|Skill")
+    FGameplayTag SkillTag;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SagoMagic|Skill")
+    FGameplayTag SkillEffectCueTag;
+
+    //** 수치들*/
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
+    float BaseDamage = 30.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
+    float RangeCm = 300.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
+    float CooldownSeconds= 3.f;
+
+    // TODO:마우스 정보 데이터 가져오기
 };

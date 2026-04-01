3947e7fc4bc05a08a370bc68b506b417d94b0300
limyt0 <dudx45@gmail.com>
2026-04-01 16:17:09 +0900

    add: MonsterBase class

---
+++ Source/SagoMagic/Enemy/MonsterBase.cpp
@@ -0,0 +1,26 @@
+﻿#include "Enemy/MonsterBase.h"
+
+AMonsterBase::AMonsterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMonsterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMonsterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMonsterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/SagoMagic/Enemy/MonsterBase.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MonsterBase.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API AMonsterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMonsterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

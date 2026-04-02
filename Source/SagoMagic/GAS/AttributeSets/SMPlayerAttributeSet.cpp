1fcd180657306166baa2ce149d36ce4ce58dc378
Park WonJong <kidsmon@naver.com>
2026-04-02 19:12:19 +0900

    update: add Character's Attribute Sets

--- Source/SagoMagic/GAS/AttributeSets/SMPlayerAttributeSet.cpp
+++ Source/SagoMagic/GAS/AttributeSets/SMPlayerAttributeSet.cpp
@@ -1,4 +1,78 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// SMPlayerAttributeSet.cpp
 
 
 #include "SMPlayerAttributeSet.h"
+
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+USMPlayerAttributeSet::USMPlayerAttributeSet()
+{
+	// TODO: GameplayEffect 또는 DataAsset으로 초기화 고려
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitGold(0.0f);
+	
+	// 참고: AttributeSet특성상 32비트 float로 돼있기 때문에 1600만이 최대
+	InitMaxGold(1000000.0f);
+}
+
+void USMPlayerAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(USMPlayerAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USMPlayerAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USMPlayerAttributeSet, Gold, COND_None, REPNOTIFY_Always);
+}
+
+void USMPlayerAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USMPlayerAttributeSet, Health, OldHealth);
+}
+
+void USMPlayerAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USMPlayerAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void USMPlayerAttributeSet::OnRep_Gold(const FGameplayAttributeData& OldGold)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USMPlayerAttributeSet, Gold, OldGold);
+}
+
+void USMPlayerAttributeSet::OnRep_MaxGold(const FGameplayAttributeData& OldMaxGold)
+{
+}
+
+void USMPlayerAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+
+	if (Attribute == GetGoldAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxGold());
+	}
+}
+
+void USMPlayerAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+
+		// TODO: Health가 0이되면 Death처리 후 UnPosses처리
+	}
+
+	if (Data.EvaluatedData.Attribute == GetGoldAttribute())
+	{
+		SetGold(FMath::Clamp(GetGold(), 0.0f, GetMaxGold()));
+	}
+}
--- Source/SagoMagic/GAS/AttributeSets/SMPlayerAttributeSet.h
+++ Source/SagoMagic/GAS/AttributeSets/SMPlayerAttributeSet.h
@@ -1,17 +1,67 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// SMPlayerAttributeSet.h
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 
 #include "SMPlayerAttributeSet.generated.h"
 
+#define ATTRIBUTE_ACCESSORS(ClassName,PropertyName)\
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName,PropertyName)\
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)\
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)\
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 /**
- *
+ * 플레이어의 스탯을 관리하는 AttributeSet
  */
 UCLASS()
 class SAGOMAGIC_API USMPlayerAttributeSet : public UAttributeSet
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
+
+public:
+	USMPlayerAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/** Player Health */
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Health", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(USMPlayerAttributeSet, Health)
+
+	/** Player MaxHealth */
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|MaxHealth", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(USMPlayerAttributeSet, MaxHealth)
+
+	/** Player Gold */
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Gold", ReplicatedUsing = OnRep_Gold)
+	FGameplayAttributeData Gold;
+	ATTRIBUTE_ACCESSORS(USMPlayerAttributeSet, Gold)
+
+	/** Player Gold */
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Gold", ReplicatedUsing = OnRep_MaxGold)
+	FGameplayAttributeData MaxGold;
+	ATTRIBUTE_ACCESSORS(USMPlayerAttributeSet, MaxGold)
+
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	UFUNCTION()
+	virtual void OnRep_Gold(const FGameplayAttributeData& OldGold);
+
+	UFUNCTION()
+	virtual void OnRep_MaxGold(const FGameplayAttributeData& OldMaxGold);
+
+	// Attribute 변경 전 호출
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	// Attribute 변경 후 호출
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 };

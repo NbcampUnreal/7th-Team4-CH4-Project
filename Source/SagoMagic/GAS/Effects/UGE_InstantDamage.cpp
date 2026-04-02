8d422e4c4fffb45ea8b1ec7868a07520fd0ec5e9
허태린 <ouranos0902@gmail.com>
2026-04-03 01:17:06 +0900

    add: InstantDamage Class

---
+++ Source/SagoMagic/GAS/Effects/UGE_InstantDamage.cpp
@@ -0,0 +1,1 @@
+﻿#include "UGE_InstantDamage.h"
---
+++ Source/SagoMagic/GAS/Effects/UGE_InstantDamage.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UGE_SkillDamage.h"
+#include "UGE_InstantDamage.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SAGOMAGIC_API UUGE_InstantDamage : public UGE_SkillDamage
+{
+    GENERATED_BODY()
+};

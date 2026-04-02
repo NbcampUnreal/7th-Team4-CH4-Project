011de332d85fdece431552542933491ba171632c
Park WonJong <kidsmon@naver.com>
2026-04-03 00:21:50 +0900

    update: implement SMPlayerState

--- Source/SagoMagic/Core/SMPlayerState.cpp
+++ Source/SagoMagic/Core/SMPlayerState.cpp
@@ -2,3 +2,23 @@
 
 
 #include "SMPlayerState.h"
+
+#include "AbilitySystemComponent.h"
+#include "GAS/AttributeSets/SMPlayerAttributeSet.h"
+
+ASMPlayerState::ASMPlayerState()
+{
+	SMAbilitySystemComponent = CreateDefaultSubobject<USMAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	
+	SMAbilitySystemComponent->SetIsReplicated(true);
+	SMAbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Full);
+	
+	AttributeSet = CreateDefaultSubobject<USMPlayerAttributeSet>(TEXT("AttributeSet"));
+	
+	SetNetUpdateFrequency(100.0f);
+}
+
+UAbilitySystemComponent* ASMPlayerState::GetAbilitySystemComponent() const
+{
+	return SMAbilitySystemComponent;
+}

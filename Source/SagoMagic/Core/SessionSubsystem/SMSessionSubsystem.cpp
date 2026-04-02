44ab6feb2c9d68f22ed2a40b09473f0c9fd52bb5
JRoLee <cnsfw123@gmail.com>
2026-04-02 15:59:03 +0900

    Update : add function to SessionSubsystem skeleton
    Update : add function to SessionSubsystem skeleton

--- Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.cpp
+++ Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.cpp
@@ -3,31 +3,8 @@
 
 #include "SMSessionSubsystem.h"
 
-FOnSMCreateSessionComplete::FOnSMCreateSessionComplete()
-{
-}
-
-FOnSMCreateSessionComplete::FOnSMCreateSessionComplete(const TMulticastScriptDelegate<>& InMulticastScriptDelegate)
-{
-}
-
-void FOnSMCreateSessionComplete::FOnSMCreateSessionComplete_DelegateWrapper(const FMulticastScriptDelegate&,
-    bool bWasSuccessful)
-{
-}
-
-FOnSMDestroySessionComplete::FOnSMDestroySessionComplete()
-{
-}
-
-FOnSMDestroySessionComplete::FOnSMDestroySessionComplete(const TMulticastScriptDelegate<>& InMulticastScriptDelegate)
-{
-}
-
-void FOnSMDestroySessionComplete::FOnSMDestroySessionComplete_DelegateWrapper(const FMulticastScriptDelegate&,
-    bool bWasSuccessful)
-{
-}
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystem.h"
 
 USMSessionSubsystem::USMSessionSubsystem()
 {
@@ -36,43 +13,141 @@ USMSessionSubsystem::USMSessionSubsystem()
 void USMSessionSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
     Super::Initialize(Collection);
+
+    if (IsValidSessionInterface())
+    {
+        InviteAcceptedDelegateHandle = SessionInterface->AddOnSessionUserInviteAcceptedDelegate_Handle(
+            FOnSessionUserInviteAcceptedDelegate::CreateUObject(
+                this, &ThisClass::OnSessionUserInviteAcceptedInternal));
+    }
 }
 
 void USMSessionSubsystem::Deinitialize()
 {
+    if ( SessionInterface.IsValid())
+    {
+        SessionInterface->ClearOnSessionUserInviteAcceptedDelegate_Handle(
+            InviteAcceptedDelegateHandle);
+    }
     Super::Deinitialize();
 }
 
 void USMSessionSubsystem::CreateSession(int32 MaxPlayer)
 {
+    if (IsValidSessionInterface() == false) return;
+
+    if (SessionInterface->GetNamedSession(NAME_GameSession))
+    {
+        DestroySession();
+        return;
+    }
+
+    CreateSessionCompleteDelegateHandle =
+        SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(
+            FOnCreateSessionCompleteDelegate::CreateUObject(
+                this, &ThisClass::OnCreateSessionCompleteInternal));
+
+    //세션 세팅갑 설정
+    TSharedPtr<FOnlineSessionSettings> Settings = MakeShareable(new FOnlineSessionSettings());
+    bool bIsDedicated = IsRunningDedicatedServer();
+    Settings->NumPublicConnections = MaxPlayer;
+    Settings->bIsDedicated = bIsDedicated;
+    Settings->bUsesPresence = !bIsDedicated;
+    Settings->bUseLobbiesIfAvailable = !bIsDedicated;
+    Settings->bShouldAdvertise = true;
+    Settings->bAllowJoinViaPresence = !bIsDedicated;
+    Settings->bAllowJoinInProgress = false;
+    Settings->bIsLANMatch = IOnlineSubsystem::Get()->GetSubsystemName() == "NULL";
+
+    const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+    if (!SessionInterface->CreateSession(
+        *LocalPlayer->GetPreferredUniqueNetId(),NAME_GameSession, *Settings))
+    {
+        SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(
+            CreateSessionCompleteDelegateHandle);
+        OnCreateSessionComplete.Broadcast(false);
+    }
 }
 
 void USMSessionSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
 {
+    if (IsValidSessionInterface() == false)
+    {
+        OnJoinSessionComplete.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
+        return;
+    }
+
+    JoinSessionCompleteDelegateHandle = SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(
+        FOnJoinSessionCompleteDelegate::CreateUObject(
+            this, &ThisClass::OnJoinSessionCompleteInternal));
+
+    const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+
+    if (!SessionInterface->JoinSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, SessionResult))
+    {
+        SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+        OnJoinSessionComplete.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
+    }
 }
 
 void USMSessionSubsystem::DestroySession()
 {
+    if (IsValidSessionInterface() == false)
+    {
+        OnDestroySessionComplete.Broadcast(false);
+        return;
+    }
+
+    DestroySessionCompleteDelegateHandle = SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(
+        FOnDestroySessionCompleteDelegate::CreateUObject(
+            this, &ThisClass::OnDestroySessionCompleteInternal));
+
+    if (!SessionInterface->DestroySession(NAME_GameSession))
+    {
+        SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+        OnDestroySessionComplete.Broadcast(false);
+    }
 }
 
 void USMSessionSubsystem::OnCreateSessionCompleteInternal(FName SessionName, bool bWasSuccessful)
 {
+    SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(
+        CreateSessionCompleteDelegateHandle);
+    OnCreateSessionComplete.Broadcast(bWasSuccessful);
 }
 
 void USMSessionSubsystem::OnJoinSessionCompleteInternal(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
 {
+    SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(
+        JoinSessionCompleteDelegateHandle);
+    OnJoinSessionComplete.Broadcast(Result);
 }
 
 void USMSessionSubsystem::OnDestroySessionCompleteInternal(FName SessionName, bool bWasSuccessful)
 {
+    SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(
+        DestroySessionCompleteDelegateHandle);
+    OnDestroySessionComplete.Broadcast(bWasSuccessful);
 }
 
 void USMSessionSubsystem::OnSessionUserInviteAcceptedInternal(bool bWasSuccessful, int32 LocalUserNum,
     FUniqueNetIdPtr UserId, const FOnlineSessionSearchResult& InviteResult)
 {
+    if (bWasSuccessful == true)
+    {
+        JoinSession(InviteResult);
+    }
 }
 
 bool USMSessionSubsystem::IsValidSessionInterface()
 {
-    return true;
+    if (SessionInterface.IsValid() == false)
+    {
+        IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+        if (Subsystem)
+        {
+            SessionInterface = Subsystem->GetSessionInterface();
+        }
+    }
+    return SessionInterface.IsValid();
 }
--- Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.h
+++ Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.h
@@ -12,7 +12,7 @@
  * 스팀과 연동되어 친구 초대 및 수락 가능
  */
 
-/** 위젯 바인딩용 델리게이트*/
+//위젯 바인딩용 델리게이트
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSMCreateSessionComplete, bool, bWasSuccessful);
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnSMJoinSessionComplete, EOnJoinSessionCompleteResult::Type);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSMDestroySessionComplete, bool, bWasSuccessful);
@@ -23,37 +23,46 @@ class SAGOMAGIC_API USMSessionSubsystem : public UGameInstanceSubsystem
 public:
     USMSessionSubsystem();
 
-    /** 서브시스템 인터페이스 */
+    //서브 시스템 인터페이스
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
 
-    /** 위젯 호출용 */
+    //위젯 호출용
     void CreateSession(int32 MaxPlayer);
     void JoinSession(const FOnlineSessionSearchResult& SessionResult);
     void DestroySession();
 
-    /** 위젯 바인딩 델리게이트 */
-    FOnSMCreateSessionComplete OnCreateComplete;
+    //위젯 바인딩용 델리게이트
+    FOnSMCreateSessionComplete OnCreateSessionComplete;
     FOnSMJoinSessionComplete OnJoinSessionComplete;
-    FOnSMDestroySessionComplete OnDestroyComplete;
+    FOnSMDestroySessionComplete OnDestroySessionComplete;
 
 private:
     IOnlineSessionPtr SessionInterface;
 
-    /** 델리게이트 핸들 */
+    //델리게이트 핸들
     FDelegateHandle CreateSessionCompleteDelegateHandle;
     FDelegateHandle JoinSessionCompleteDelegateHandle;
     FDelegateHandle DestroySessionCompleteDelegateHandle;
     FDelegateHandle InviteAcceptedDelegateHandle;
 
-    /** 내부 콜백 (엔진 호출) */
+    //내부 콜백용 함수
+    /** 세션 생성 성공 콜백 함수 */
     void OnCreateSessionCompleteInternal(FName SessionName, bool bWasSuccessful);
+
+    /** 세션 조인 성공 콜백 함수 */
     void OnJoinSessionCompleteInternal(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+    /** 세션 파괴 성공 콜백 함수 */
     void OnDestroySessionCompleteInternal(FName SessionName, bool bWasSuccessful);
+
+    /** 초대 수락 성공 콜백 함수 */
     void OnSessionUserInviteAcceptedInternal(
         bool bWasSuccessful,
         int32 LocalUserNum,
         FUniqueNetIdPtr UserId,
         const FOnlineSessionSearchResult& InviteResult);
+
+    /** 세션 인터페이스 벨리데이션 함수 */
     bool IsValidSessionInterface();
 };

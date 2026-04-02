84ab6429f96443b165a7bc58f7af567ed8d39ce9
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-02 19:51:38 +0900

    add: C++ class for Enemy HP widget

---
+++ Source/SagoMagic/UI/SMEnemyHPBarWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
+#include "SMEnemyHPBarWidget.generated.h"
+
+
+UCLASS()
+class SAGOMAGIC_API USMEnemyHPBarWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    void UpdateHPBar(float Percent);
+    virtual void NativeTick(const FGeometry& Geometry, float DeltaTime) override;
+
+protected:
+    UPROPERTY(meta=(BindWidget))
+    UProgressBar* EnemyHPBar;
+
+    float TargetPercent = 1.0f;
+    float CurrentPercent = 1.0f;
+
+    UPROPERTY(EditAnywhere, Category = "UI Settings")
+    float InterpSpeed = 10.0f;
+};

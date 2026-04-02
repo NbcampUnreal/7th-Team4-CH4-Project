2e502b6ef0c187884c08c807c6cc0c1a0fe8f15f
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-04-02 14:20:46 +0900

    fix : Class Name Change SMWaveManagerSubsystem to SMGameFlowSubsystem

---
+++ Source/SagoMagic/Core/SMGameFlowSubSystem.cpp
@@ -0,0 +1,42 @@
+﻿#include "SMGameFlowSubSystem.h"
+
+#include "SMGameState.h"
+
+void USMGameFlowSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+}
+
+void USMGameFlowSubSystem::Deinitialize()
+{
+    //command 정리
+    if (CurrentCommand)
+        CurrentCommand->Exit(nullptr);
+    CurrentCommand.Reset();
+
+    Super::Deinitialize();
+}
+
+void USMGameFlowSubSystem::Tick(float DeltaTime)
+{
+}
+
+void USMGameFlowSubSystem::AdvanceCommand()
+{
+    if (CurrentCommand)
+        CurrentCommand->Exit(GetWorld()->GetAuthGameMode<ASMGameMode>());
+
+    TSharedPtr<IGamePhaseCommand> Next;
+    if (CommandQueue.Dequeue(Next))
+    {
+        CurrentCommand = Next;
+        CurrentCommand->OnCompleted.BindUObject(
+            this, &USMGameFlowSubSystem::AdvanceCommand);
+        CurrentCommand->Enter(GetWorld()->GetAuthGameMode<ASMGameMode>());
+    }
+
+    if (ASMGameState* GS = GetWorld()->GetGameState<ASMGameState>())
+        GS->Set
+
+
+}
--- Source/SagoMagic/Core/SMWaveManagerSubSystem.cpp
+++
@@ -1,1 +0,0 @@
-﻿#include "SMWaveManagerSubSystem.h"

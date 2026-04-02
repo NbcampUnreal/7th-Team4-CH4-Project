07ca2882f4e03b7a40b5181a656846af93c5a4c1
허태린 <ouranos0902@gmail.com>
2026-04-03 01:16:35 +0900

    add: SkillDamage Class

---
+++ Source/SagoMagic/GAS/Effects/UGE_SkillDamage.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "UGE_SkillDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAGOMAGIC_API UUGE_SkillDamage : public UGameplayEffect
+{
+    GENERATED_BODY()
+};

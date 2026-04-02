563d80231daae4126549e9088a6dd4ec64487815
limyt0 <dudx45@gmail.com>
2026-04-02 17:17:23 +0900

    add: MonsterAttributeSet class

---
+++ Source/SagoMagic/GAS/AttributeSets/SMMonsterAttributeSet.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "SMMonsterAttributeSet.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API USMMonsterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};

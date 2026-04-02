17ae3515f3a680aa5b972c87639092c084968934
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-02 10:26:09 +0900

    add : SystemLogData Setting

---
+++ Source/SagoMagic/Data/SMLogData.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SMLogData.generated.h"
+
+
+USTRUCT()
+struct FSMLogData : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText DisplayText;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FLinearColor TextColor = FLinearColor::White;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float DisplayDuration = 2.0f;
+};

0bb0bb35088859c669c6b244645c84bc5ed15fed
Cheolmin Joo <cjfals4539@gmail.com>
2026-04-02 21:07:04 +0900

    add: SkillInventoryWidget Initialized
    SkillInventoryWidget 초기 작성

---
+++ Source/SagoMagic/UI/Inventory/SMSkillInventoryWidget.cpp
@@ -0,0 +1,35 @@
+﻿#include "UI/Inventory/SMSkillInventoryWidget.h"
+
+#include "Inventory/Components/SMInventoryComponent.h"
+
+USMSkillInventoryWidget::USMSkillInventoryWidget(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+
+}
+
+void USMSkillInventoryWidget::InitializeSkillInventoryWidget(
+    const FGuid& InSkillInstanceId,
+    const FGuid& InContainerId,
+    USMInventoryComponent* InInventoryComponent)
+{
+    SkillInstanceId = InSkillInstanceId;
+    InitializeGridWidget(InContainerId, InInventoryComponent);
+    BP_OnSkillInventoryUpdated();
+}
+
+void USMSkillInventoryWidget::ChangeTargetSkill(const FGuid& InSkillInstanceId, const FGuid& InContainerId)
+{
+    SkillInstanceId = InSkillInstanceId;
+    SetContainerId(InContainerId);
+    RefreshGrid();
+    BP_OnSkillInventoryUpdated();
+}
+
+void USMSkillInventoryWidget::ClearTargetSkill()
+{
+    SkillInstanceId.Invalidate();
+    SetContainerId(FGuid());
+    RefreshGrid();
+    BP_OnSkillInventoryUpdated();
+}

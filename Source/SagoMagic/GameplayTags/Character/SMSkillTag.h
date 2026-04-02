f415340bd038de073622a5ba5f240a743800a63d
허태린 <ouranos0902@gmail.com>
2026-04-02 17:19:16 +0900

    add: Add SkillTag
    ^ Conflicts:
    ^	Source/SagoMagic/GameplayTags/Character/SMSkillTag.cpp
    ^	Source/SagoMagic/GameplayTags/Character/SMSkillTag.h

--- Source/SagoMagic/GameplayTags/Character/SMSkillTag.h
+++ Source/SagoMagic/GameplayTags/Character/SMSkillTag.h
@@ -13,35 +13,34 @@ namespace SMSkillTag
 
     //태그명 추천받습니다.
 
-    //** 스킬 태그 */
-    //**투사체*/
+    //스킬 태그들
+    //투사체
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_Projectile)
-    //**장판*/
+    //장판
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_SpawnField)
-    //**빔*/
+    //빔
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_LineTrace)
-    //**레이*/
+    //레이
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_ApplyInstantDamage)
 
-    //**쿨다운 태그들*/
+    //쿨다운 태그들
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Skill_Projectile)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Skill_SpawnField)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Skill_LineTrace)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Skill_ApplyInstantDamage)
 
-    //**Set By Caller 데이터 태그*/
-    //**UGE_SkillDamage 에서 피해량*/
+    //Set By Caller 데이터 태그
+    //UGE_SkillDamage 에서 피해량
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Damage_Amount)
-    //**UGE_SkillCooldown - Duration*/
+    //UGE_SkillCooldown - Duration
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Cooldown)
 
-    //**아군 태그 - 아군의 공격이 아군이나 HQ, 아군의 구조물을 임하지 못하게*/
+    //아군 태그 - 아군의 침노가 아군이나 HQ를 임하지 못하게
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Team_Player)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Team_HQ)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(Team_Building)
 
-
-    //**Cue 태그*/
+    //코스메틱 태그
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayCue_Skill_Projectile_Hit)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayCue_Skill_SpawnField_Tick)
     UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayCue_Skill_LineTrace_Hit)

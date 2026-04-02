99533b22fc8a0b54fd3222cacfa3a324685adc0e
Cheolmin Joo <cjfals4539@gmail.com>
2026-04-02 21:07:25 +0900

    add: SMQuickSlotBarWidget Initialized
    SMQuickSlotBarWidget 초기 작성

---
+++ Source/SagoMagic/UI/Inventory/SMQuickSlotBarWidget.cpp
@@ -0,0 +1,66 @@
+﻿#include "UI/Inventory/SMQuickSlotBarWidget.h"
+
+#include "Inventory/Components/SMInventoryComponent.h"
+#include "Inventory/Core/SMContainerTypes.h"
+
+USMQuickSlotBarWidget::USMQuickSlotBarWidget(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+    , InventoryComponent(nullptr)
+    , ActiveSlotIndex(0)
+{
+}
+
+void USMQuickSlotBarWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+}
+
+void USMQuickSlotBarWidget::InitializeQuickSlotBarWidget(USMInventoryComponent* InInventoryComponent)
+{
+    InventoryComponent = InInventoryComponent;
+    RefreshQuickSlotBar();
+}
+
+void USMQuickSlotBarWidget::RefreshQuickSlotBar()
+{
+    SyncFromInventoryComponent();
+    BP_OnQuickSlotBarUpdated();
+}
+
+void USMQuickSlotBarWidget::ActivateFirstSlot()
+{
+    if (InventoryComponent == nullptr)
+    {
+        return;
+    }
+
+    InventoryComponent->SetActiveQuickSlot(0);
+    RefreshQuickSlotBar();
+}
+
+void USMQuickSlotBarWidget::ActivateSecondSlot()
+{
+    if (InventoryComponent == nullptr)
+    {
+        return;
+    }
+
+    InventoryComponent->SetActiveQuickSlot(1);
+    RefreshQuickSlotBar();
+}
+
+void USMQuickSlotBarWidget::SyncFromInventoryComponent()
+{
+    if (InventoryComponent == nullptr)
+    {
+        Slot1SkillId.Invalidate();
+        Slot2SkillId.Invalidate();
+        ActiveSlotIndex = 0;
+        return;
+    }
+
+    const FSMQuickSlotSetState& QuickSlots = InventoryComponent->GetQuickSlots();
+    Slot1SkillId = QuickSlots.Slot1SkillId;
+    Slot2SkillId = QuickSlots.Slot2SkillId;
+    ActiveSlotIndex = QuickSlots.ActiveSlotIndex;
+}

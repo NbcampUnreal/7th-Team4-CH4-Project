d0744e7b5d67e0a2c8da4100ec439a7b25480cd5
허태린 <ouranos0902@gmail.com>
2026-04-03 01:17:32 +0900

    comment: Add Comment

--- Source/SagoMagic/GAS/Effects/UGE_SkillDamage.cpp
+++ Source/SagoMagic/GAS/Effects/UGE_SkillDamage.cpp
@@ -1,4 +1,6 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "UGE_SkillDamage.h"
 
-
-#include "UGE_SkillDamage.h"
+UGE_SkillDamage::UGE_SkillDamage()
+{
+    //InstantDamage에서 DurationPolicy, Modifiers, GameplayCues 설정
+}
--- Source/SagoMagic/GAS/Effects/UGE_SkillDamage.h
+++ Source/SagoMagic/GAS/Effects/UGE_SkillDamage.h
@@ -7,10 +7,14 @@
 #include "UGE_SkillDamage.generated.h"
 
 /**
- * 
+ * 스킬 피해 InstantDamage 상위 클래스
+ * 직접 사용하지않고 InstantDamage를 통해 사용
  */
 UCLASS()
-class SAGOMAGIC_API UUGE_SkillDamage : public UGameplayEffect
+class SAGOMAGIC_API UGE_SkillDamage : public UGameplayEffect
 {
     GENERATED_BODY()
+
+public:
+    UGE_SkillDamage();
 };

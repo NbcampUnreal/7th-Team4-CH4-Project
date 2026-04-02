b6f731936145e1b9c4c27535a48abd7842360b33
허태린 <ouranos0902@gmail.com>
2026-04-02 19:14:37 +0900

    fix: SkillCooldown class code
    스킬 쿨다운 클래스 코드 작성

--- Source/SagoMagic/GAS/Effects/UGE_SkillCooldown.cpp
+++ Source/SagoMagic/GAS/Effects/UGE_SkillCooldown.cpp
@@ -1,4 +1,18 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "UGE_SkillCooldown.h"
+#include "GameplayTags/Character/SMSkillTag.h"
 
+UGE_SkillCooldown::UGE_SkillCooldown()
+{
+    DurationPolicy = EGameplayEffectDurationType::HasDuration;
 
-#include "UGE_SkillCooldown.h"
+    // Duration 은 UGA_SkillBase::ApplyCooldown() 에서 SetByCaller로 주입.
+    FSetByCallerFloat SetByCallerDuration;
+    SetByCallerDuration.DataTag = SMSkillTag::Data_Cooldown;
+    //지속시간 수치 대입, Caller에서 정해주는 값에 따라 결정.
+    DurationMagnitude = FGameplayEffectModifierMagnitude(SetByCallerDuration);
+
+    //쿨다운이 걸려 있을 때 중첩되면 최대 중첩 횟수 1로 고정, 다시 걸리면 시간 초기화.
+    StackingType               = EGameplayEffectStackingType::AggregateByTarget;
+    StackLimitCount            = 1;
+    StackDurationRefreshPolicy = EGameplayEffectStackingDurationPolicy::RefreshOnSuccessfulApplication;
+}
--- Source/SagoMagic/GAS/Effects/UGE_SkillCooldown.h
+++ Source/SagoMagic/GAS/Effects/UGE_SkillCooldown.h
@@ -1,16 +1,21 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameplayEffect.h"
 #include "UGE_SkillCooldown.generated.h"
 
 /**
- * 
+ * HasDuration
+ *
+ * Duration을 SetByCaller(Data.Cooldown)으로 받아 n초 동안 유지
+ * SKillBase::ApplyCooldown에서 CooldownSeconds 값을 넣어서 적용
+ *
+ * 각 GA의 ActivationBlockedTags에 해당하는 Cooldown.Skill.스킬타입 태그를 등록하면
+ * 이 이펙트가 살아있는 동안 스킬 재상용이 자동으로 차단된다
  */
 UCLASS()
-class SAGOMAGIC_API UUGE_SkillCooldown : public UGameplayEffect
+class SAGOMAGIC_API UGE_SkillCooldown : public UGameplayEffect
 {
     GENERATED_BODY()
+    UGE_SkillCooldown();
 };

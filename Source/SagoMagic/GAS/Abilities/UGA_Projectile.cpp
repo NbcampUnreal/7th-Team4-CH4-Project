b1e73708dc8f4f84fb0a0f871e877f02f60611ea
허태린 <ouranos0902@gmail.com>
2026-04-03 01:55:58 +0900

    add: Add UGA_Projectile Class

--- Source/SagoMagic/GAS/Abilities/UGA_Projectile.cpp
+++ Source/SagoMagic/GAS/Abilities/UGA_Projectile.cpp
@@ -1,1 +1,32 @@
 ﻿#include "UGA_Projectile.h"
+
+UGA_Projectile::UGA_Projectile()
+{
+    ProjectileClass = SMASkillProjectile::StaticClass();
+}
+
+void UGA_Projectile::OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo)
+{
+    APawn* Avatar = ActorInfo && ActorInfo->AvatarActor.IsValid()
+        ? Cast<APawn>(ActorInfo->AvatarActor.Get()) : nullptr;
+    if (!Avatar || !Avatar->HasAuthority()) return;
+
+    UWorld* World = GetWorld();
+    if (!World || !ProjectileClass) return;
+
+    // 캐릭터 앞 50cm 에서 발사
+    const FVector SpawnLocation = CurrentAimOrigin + CurrentAimDirection * 50.f;
+
+    FActorSpawnParameters Params;
+    Params.Owner      = Avatar;
+    Params.Instigator = Avatar;
+    Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    //에디터에서 설정한 클래스 사용
+    ASkillProjectile* Proj = World->SpawnActor<ASkillProjectile>(ProjectileClass, SpawnLocation, CurrentAimDirection.Rotation(), Params);
+
+    if (Proj)
+    {
+        Proj->InitProjectile(BaseDamage, RangeCm, CurrentAimDirection,Avatar, Avatar->GetController(), DamageEffectClass);
+    }
+}
--- Source/SagoMagic/GAS/Abilities/UGA_Projectile.h
+++ Source/SagoMagic/GAS/Abilities/UGA_Projectile.h
@@ -1,6 +1,5 @@
 ﻿#pragma once
 
-#include "CoreMinimal.h"
 #include "UGA_SkillBase.h"
 #include "UGA_Projectile.generated.h"
 
@@ -8,7 +7,17 @@
  *
  */
 UCLASS()
-class SAGOMAGIC_API UUGA_Projectile : public UGA_SkillBase
+class SAGOMAGIC_API UGA_Projectile : public UGA_SkillBase
 {
     GENERATED_BODY()
+
+public:
+    UGA_Projectile();
+
+protected:
+    virtual void OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo) override;
+
+    //TODO : 풀리퀘 하고 나서 주석 풀기
+    //UPROPERTY(EditDefaultsOnly, Category = "Skill")
+    //TSubclassOf<SMASkillProjectile> ProjectileClass;
 };

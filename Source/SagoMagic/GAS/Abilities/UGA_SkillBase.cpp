9f325f319e922d9d6d8ee04fda05c122b4973473
허태린 <ouranos0902@gmail.com>
2026-04-02 16:16:17 +0900

    update: SkillBase Skeleta Code
    마우스 정보 가져오기 - ExtractAimData
    쿨타임 관리 - ApplyCooldown 쿨타임 0.5초로 설정
    쿨타운중 태그로 관리 - SetSetByCallerMagnitude
    
    현재 GA에 있는 스탯들은 DA가 생성되지 않아서 임시로 넣은 스탯들

--- Source/SagoMagic/GAS/Abilities/UGA_SkillBase.cpp
+++ Source/SagoMagic/GAS/Abilities/UGA_SkillBase.cpp
@@ -1,5 +1,7 @@
 #include "GAS/Abilities/UGA_SkillBase.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayTags/Character/SMSkillTag.h"
+#include "GameFramework/Character.h"
 
 UGA_SkillBase::UGA_SkillBase()
 {
@@ -9,21 +11,67 @@ UGA_SkillBase::UGA_SkillBase()
     NetExecutionPolicy =EGameplayAbilityNetExecutionPolicy::LocalPredicted;
     //인스턴스를 네트워크로 복제할지 - 복제안함
     ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
+    //쿨다운 GE 설정 - 에디터
+    //CooldownGameplayEffectClass = UGE_SkillCooldown::StaticClass;
 }
 
 bool UGA_SkillBase::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
 {
+    //GAS의 기본조건을 자동을 체크 (Ability, ActivationBlockedTags 등)
     return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
 }
 
 void UGA_SkillBase::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+    if (!ActorInfo || !ActorInfo->AvatarActor.IsValid())
+    {
+        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+        return;
+    }
+
+    // 마우스 정보 가져오기, 커서 방향 -> CurrentAimOrigin, CurrentAimDirection
+    ExtractAimData(ActorInfo);
+
+    if (!CommitAbility(Handle,ActorInfo,ActivationInfo))
+    {
+        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+        return;
+    }
+
+    // TODO: HasAuthority 체크 요망
+    OnSkillEffect(ActorInfo);
+
+    EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
 
 void UGA_SkillBase::ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
 {
-    Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+    //CooldownGameplatEffectClass가 없거나 쿨다운이 0이면 패스
+    if (CooldownGameplayEffectClass || CooldownSeconds <=0.f)
+    {
+        Super::ApplyCooldown(Handle, ActorInfo, ActivationInfo);
+        return;
+    }
+
+    //GE Spec 생성
+    FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(CooldownGameplayEffectClass, GetAbilityLevel());
+    if (!SpecHandle.IsValid()) return;
+
+    //TODO: UGE_SkillCooldown 만들어서 SetByCaller 이용 CooldownSeconds 전달
+    //TODO: CooldownSeconds가 0 초과일때만 적용할 수 있는지 디버깅 필요
+    //SetByCaller로 쿨다운 시간 주입.
+    SpecHandle.Data->SetSetByCallerMagnitude(SMSkillTag::Data_Cooldown, CooldownSeconds);
+
+    //스킬별 쿨다운 태그 추가
+    //GA에 ActivationBlockTags에 같은 태그를 등록하면 쿨다운 중 스킬 발동 불가하는 식으로
+    if (CooldownTag.IsValid())
+    {
+        SpecHandle.Data->DynamicGrantedTags.AddTag(CooldownTag);
+    }
+
+    //TODO : SetByCaller 확인
+    //동적으로 쿨다운을 적용시켜주는 함수
+    ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, SpecHandle);
 }
 
 
@@ -37,8 +85,29 @@ FString UGA_SkillBase::GetCurrentPredictionKeyStatus()
 
 bool UGA_SkillBase::IsPredictionKeyValidForMorePrediction() const
 {
+    //스킬을 방동하는 순간 Prediction Key가 생성됨 이하 예측 키, 또한 Ability Prediction Window - 유효시간이 생김.
+    //예측 키는 어빌리티 활성화-> 타겟 데이터 전송 -> 코스메틱 효과 등을 한번에 처리 이것이 More Prediction
+    //Prediction Key 만료 - 너무 많은 행동을 하면 서버에서 Acknowledgment하고 되서 만료가 되버림
+    //Prediction Key가 만료가 되면 IsValidForMorePrediction()에서 false를 반환하고
+    //새로운 Prediction Key를 생성하거나 서버의 허락을 기다려야함.
     //현재 예측 키 가 추가적인 예측을 더 할 수 있는 상태인지 bool값으로 반환.
     UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
     return ASC->ScopedPredictionKey.IsValidForMorePrediction();
 }
 
+void UGA_SkillBase::ExtractAimData(const FGameplayAbilityActorInfo* ActorInfo)
+{
+    if (!ActorInfo || !ActorInfo->AvatarActor.IsValid()) return;
+
+    ACharacter* SMCharacter = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+    if (!SMCharacter) return;
+
+    //캐릭터 위치
+    CurrentAimOrigin = SMCharacter->GetActorLocation();
+
+    //커서가 가리키는 벡터 방향 설정하기
+    if (AController* Controller = SMCharacter->GetController())
+    {
+        CurrentAimDirection = Controller->GetControlRotation().Vector();
+    }
+}
--- Source/SagoMagic/GAS/Abilities/UGA_SkillBase.h
+++ Source/SagoMagic/GAS/Abilities/UGA_SkillBase.h
@@ -23,7 +23,7 @@ public:
 
     virtual void ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
 
-    //** 현재 Prediction Key를 반환하고 더 많은? 예측에 유효한지 Vaild 한다 ,Ability Prediction Window 디버깅에 사용 */
+    //** 현재 Prediction Key를 반환하고 더 많은? 예측에 유효한지 Vaild 한다 - 디버깅에 사용 */
     UFUNCTION(BlueprintCallable, Category = "SagoMagic|Ability")
     virtual FString GetCurrentPredictionKeyStatus();
 
@@ -34,23 +34,31 @@ public:
 protected:
 
     //스킬효과 서버
-    virtual void OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo) {}
+    virtual void OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo){}
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SagoMagic|Skill")
     FGameplayTag SkillTag;
 
+    //** 쿨다운중 쿨다운 재발동 차단용
+    // ex) Cooldown.Skill.Projectile*/
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SagoMagic|Skill")
-    FGameplayTag SkillEffectCueTag;
+    FGameplayTag CooldownTag;
 
-    //수치-----
+    //** 수치들 나중에 DA로 옮길 예정, 아직 스켈레탈 코드라서 */
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
     float BaseDamage = 30.f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
     float RangeCm = 300.f;
-
+    /** 쿨타임 0.5초로 설정*/
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SagoMagic|Stats")
-    float CooldownSeconds= 3.f;
+    float CooldownSeconds= 0.5f;
 
-    // TODO:마우스 정보 데이터 가져오기
+    FVector CurrentAimOrigin = FVector::ZeroVector;
+    FVector CurrentAimDirection = FVector::ForwardVector;
+
+private:
+
+    // 마우스 정보
+    void ExtractAimData(const FGameplayAbilityActorInfo* ActorInfo);
 };

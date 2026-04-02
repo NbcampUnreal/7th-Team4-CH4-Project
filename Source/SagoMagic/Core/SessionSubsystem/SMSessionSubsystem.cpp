ebd97faabe503910195cf369d5eb59ab3e317d97
JRoLee <cnsfw123@gmail.com>
2026-04-02 12:53:04 +0900

    add : SessionSubsystem class and GameInstance class
    add : SessionSubsystem class and GameInstance class

---
+++ Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.cpp
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SMSessionSubsystem.h"
+
+FOnSMCreateSessionComplete::FOnSMCreateSessionComplete()
+{
+}
+
+FOnSMCreateSessionComplete::FOnSMCreateSessionComplete(const TMulticastScriptDelegate<>& InMulticastScriptDelegate)
+{
+}
+
+void FOnSMCreateSessionComplete::FOnSMCreateSessionComplete_DelegateWrapper(const FMulticastScriptDelegate&,
+    bool bWasSuccessful)
+{
+}
+
+FOnSMDestroySessionComplete::FOnSMDestroySessionComplete()
+{
+}
+
+FOnSMDestroySessionComplete::FOnSMDestroySessionComplete(const TMulticastScriptDelegate<>& InMulticastScriptDelegate)
+{
+}
+
+void FOnSMDestroySessionComplete::FOnSMDestroySessionComplete_DelegateWrapper(const FMulticastScriptDelegate&,
+    bool bWasSuccessful)
+{
+}
+
+USMSessionSubsystem::USMSessionSubsystem()
+{
+}
+
+void USMSessionSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+}
+
+void USMSessionSubsystem::Deinitialize()
+{
+    Super::Deinitialize();
+}
+
+void USMSessionSubsystem::CreateSession(int32 MaxPlayer)
+{
+}
+
+void USMSessionSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void USMSessionSubsystem::DestroySession()
+{
+}
+
+void USMSessionSubsystem::OnCreateSessionCompleteInternal(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void USMSessionSubsystem::OnJoinSessionCompleteInternal(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void USMSessionSubsystem::OnDestroySessionCompleteInternal(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void USMSessionSubsystem::OnSessionUserInviteAcceptedInternal(bool bWasSuccessful, int32 LocalUserNum,
+    FUniqueNetIdPtr UserId, const FOnlineSessionSearchResult& InviteResult)
+{
+}
+
+bool USMSessionSubsystem::IsValidSessionInterface()
+{
+    return true;
+}
---
+++ Source/SagoMagic/Core/SessionSubsystem/SMSessionSubsystem.h
@@ -0,0 +1,59 @@
+﻿// SMSessionSubsystem.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "SMSessionSubsystem.generated.h"
+
+/**
+ * 세션 생성및 연결을 도와주는 서브시스템
+ * 스팀과 연동되어 친구 초대 및 수락 가능
+ */
+
+/** 위젯 바인딩용 델리게이트*/
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSMCreateSessionComplete, bool, bWasSuccessful);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnSMJoinSessionComplete, EOnJoinSessionCompleteResult::Type);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSMDestroySessionComplete, bool, bWasSuccessful);
+UCLASS()
+class SAGOMAGIC_API USMSessionSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+public:
+    USMSessionSubsystem();
+
+    /** 서브시스템 인터페이스 */
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    /** 위젯 호출용 */
+    void CreateSession(int32 MaxPlayer);
+    void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+    void DestroySession();
+
+    /** 위젯 바인딩 델리게이트 */
+    FOnSMCreateSessionComplete OnCreateComplete;
+    FOnSMJoinSessionComplete OnJoinSessionComplete;
+    FOnSMDestroySessionComplete OnDestroyComplete;
+
+private:
+    IOnlineSessionPtr SessionInterface;
+
+    /** 델리게이트 핸들 */
+    FDelegateHandle CreateSessionCompleteDelegateHandle;
+    FDelegateHandle JoinSessionCompleteDelegateHandle;
+    FDelegateHandle DestroySessionCompleteDelegateHandle;
+    FDelegateHandle InviteAcceptedDelegateHandle;
+
+    /** 내부 콜백 (엔진 호출) */
+    void OnCreateSessionCompleteInternal(FName SessionName, bool bWasSuccessful);
+    void OnJoinSessionCompleteInternal(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+    void OnDestroySessionCompleteInternal(FName SessionName, bool bWasSuccessful);
+    void OnSessionUserInviteAcceptedInternal(
+        bool bWasSuccessful,
+        int32 LocalUserNum,
+        FUniqueNetIdPtr UserId,
+        const FOnlineSessionSearchResult& InviteResult);
+    bool IsValidSessionInterface();
+};

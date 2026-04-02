21eef9e5a500794fd776ad288bf8e81c824203bd
허태린 <ouranos0902@gmail.com>
2026-04-02 18:51:18 +0900

    add : SkillCooldown class create
    스킬 쿨다운 클래스 생성

---
+++ Source/SagoMagic/GAS/Effects/UGE_SkillCooldown.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "UGE_SkillCooldown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAGOMAGIC_API UUGE_SkillCooldown : public UGameplayEffect
+{
+    GENERATED_BODY()
+};

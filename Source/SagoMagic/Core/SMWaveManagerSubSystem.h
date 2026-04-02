5895dac80cb36ec04477380ae91c4562b8f7d030
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-04-02 10:47:31 +0900

    add : WaveManagerSubsystem add

---
+++ Source/SagoMagic/Core/SMWaveManagerSubSystem.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "SMWaveManagerSubSystem.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SAGOMAGIC_API USMWaveManagerSubSystem : public UWorldSubsystem
+{
+    GENERATED_BODY()
+};

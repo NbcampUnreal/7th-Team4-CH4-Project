481efcfc19e2b759b82bfd61327a13fd338cabea
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-02 19:51:49 +0900

    add: Enemy HP component to manage health logic

---
+++ Source/SagoMagic/UI/SMEnemyHPBarComponent.cpp
@@ -0,0 +1,50 @@
+﻿#include "SMEnemyHPBarComponent.h"
+#include "UI/SMEnemyHPBarWidget.h"
+#include "TimerManager.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
+
+USMEnemyHPBarComponent::USMEnemyHPBarComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+
+    SetWidgetSpace(EWidgetSpace::World);
+    SetDrawAtDesiredSize(true);
+}
+
+
+void USMEnemyHPBarComponent::BeginPlay()
+{
+    Super::BeginPlay();
+    SetVisibility(false);
+
+    /** GAS - 데이터 받은 후 작업 */
+}
+
+
+void USMEnemyHPBarComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (GetNetMode() == NM_DedicatedServer) return;
+
+    if (IsVisible())
+    {
+        APlayerController* PC = GetWorld()->GetFirstPlayerController();
+        if (PC && PC->PlayerCameraManager)
+        {
+            FVector CameraLocation = PC->PlayerCameraManager->GetCameraLocation();
+            FVector widgetLocation = GetComponentLocation();
+
+            FRotator NewRot = (CameraLocation - widgetLocation).Rotation();
+            SetWorldRotation(NewRot);
+        }
+    }
+}
+
+void USMEnemyHPBarComponent::HideHPBar()
+{
+    SetVisibility(false);
+}
---
+++ Source/SagoMagic/UI/SMEnemyHPBarComponent.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "GameplayEffectTypes.h"
+#include "SMEnemyHPBarComponent.generated.h"
+
+
+UCLASS()
+class SAGOMAGIC_API USMEnemyHPBarComponent : public UWidgetComponent
+{
+    GENERATED_BODY()
+
+public:
+    USMEnemyHPBarComponent();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+        FActorComponentTickFunction* ThisTickFunction) override;
+
+    /** GAS - 데이터 받은 후 작업 */
+    /*UFUNCTION()
+    void OnHPChanged(const FOnAttributeChangeData& Data);*/
+
+private:
+    /** 일정 시간 후 UI 숨기는 함수 */
+    void HideHPBar();
+    FTimerHandle HideTimerHandle;
+
+    UPROPERTY(EditAnywhere, Category = "UI", meta = (AllowPrivateAccess = "true"))
+    float DisplayDuration = 2.0f;
+
+    /** GAS - 데이터 받은 후 작업 */
+    /*UPROPERTY()
+    class UAbilitySystemComponent* ASC;*/
+};

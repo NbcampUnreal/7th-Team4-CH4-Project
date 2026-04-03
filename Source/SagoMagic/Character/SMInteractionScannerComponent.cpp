2e972de760b234c6223648d618382bdb99500d10
Park WonJong <kidsmon@naver.com>
2026-04-03 22:18:17 +0900

    add: new SMInteractionScannerComponent class

---
+++ Source/SagoMagic/Character/SMInteractionScannerComponent.cpp
@@ -0,0 +1,119 @@
+// SMInteractionScannerComponent.cpp
+
+
+#include "SMInteractionScannerComponent.h"
+
+#include "Components/SMInteractionTargetComponent.h"
+
+
+USMInteractionScannerComponent::USMInteractionScannerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// 기본 스캔 반경(에디터 수정 가능)
+	InitSphereRadius(300.0f);
+	// 충돌 채널 세팅(에디터 수정 가능)
+	SetCollisionProfileName(TEXT("Trigger"));
+}
+
+void USMInteractionScannerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnScannerBeginOverlap);
+	OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnScannerEndOverlap);
+}
+
+void USMInteractionScannerComponent::TickComponent(
+	float DeltaTime,
+	ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 배열이 비어 있으면 불끄고 종료
+	if (OverlappedTargets.IsEmpty())
+	{
+		if (CurrentFocusedTarget)
+		{
+			CurrentFocusedTarget->SetFocusedLocally(false);
+			CurrentFocusedTarget = nullptr;
+		}
+		
+		return;
+	}
+	
+	USMInteractionTargetComponent* ClosestTarget = FindClosestTarget();
+	
+	// 1등이 바뀔때 변경 로직
+	if (ClosestTarget != CurrentFocusedTarget)
+	{
+		// 전 1등 불끄기
+		if (CurrentFocusedTarget)
+		{
+			CurrentFocusedTarget->SetFocusedLocally(false);
+		}
+		
+		// 새로운 1등 불 켜기
+		if (ClosestTarget)
+		{
+			ClosestTarget->SetFocusedLocally(true);
+		}
+		
+		CurrentFocusedTarget = ClosestTarget;
+	}
+}
+
+void USMInteractionScannerComponent::OnScannerBeginOverlap(
+	UPrimitiveComponent* OverlapComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSeep,
+	const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == GetOwner()) return;
+	
+	USMInteractionTargetComponent* TargetComp = OtherActor->FindComponentByClass<USMInteractionTargetComponent>();
+	if (TargetComp)
+	{
+		OverlappedTargets.AddUnique(TargetComp);
+	}
+}
+
+void USMInteractionScannerComponent::OnScannerEndOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (!OtherActor) return;
+	
+	USMInteractionTargetComponent* TargetComp = OtherActor->FindComponentByClass<USMInteractionTargetComponent>();
+	if (TargetComp)
+	{
+		OverlappedTargets.Remove(TargetComp);
+	}
+}
+
+USMInteractionTargetComponent* USMInteractionScannerComponent::FindClosestTarget() const
+{
+	USMInteractionTargetComponent* BestTarget = nullptr;
+	float MinDistanceSquared = MAX_FLT;
+	FVector MyLocation = GetComponentLocation();
+	
+	for (USMInteractionTargetComponent* Target : OverlappedTargets)
+	{
+		if (!IsValid(Target)) continue;
+		
+		float DistSquared = FVector::DistSquared(MyLocation, Target->GetOwner()->GetActorLocation());
+		
+		if (DistSquared < MinDistanceSquared)
+		{
+			MinDistanceSquared = DistSquared;
+			BestTarget = Target;
+		}
+	}
+	
+	return BestTarget;
+}
---
+++ Source/SagoMagic/Character/SMInteractionScannerComponent.h
@@ -0,0 +1,59 @@
+// SMInteractionScannerComponent.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "SMInteractionScannerComponent.generated.h"
+
+class USMInteractionTargetComponent;
+/**
+ * 플레이어에게 부착되어 주변의 상호작용 가능한 대상을 스캔하고
+ * 가장 가까운 대상에게 하이라이트 시키는 컴포넌트
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SAGOMAGIC_API USMInteractionScannerComponent : public USphereComponent
+{
+	GENERATED_BODY()
+
+public:
+	USMInteractionScannerComponent();
+
+	virtual void TickComponent(
+		float DeltaTime,
+		ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	USMInteractionTargetComponent* GetClosestTarget() const { return CurrentFocusedTarget; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnScannerBeginOverlap(
+		UPrimitiveComponent* OverlapComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSeep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnScannerEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+private:
+	USMInteractionTargetComponent* FindClosestTarget() const;
+	
+	/** 댕글링 포인터를 막기 위한 리플렉션 */
+	UPROPERTY()
+	TArray<TObjectPtr<USMInteractionTargetComponent>> OverlappedTargets;
+
+	/** 댕글링 포인터를 막기 위한 리플렉션 */
+	UPROPERTY()
+	TObjectPtr<USMInteractionTargetComponent> CurrentFocusedTarget;
+};

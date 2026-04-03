811c3700b3ddc34fcb4f00c41f70614e5a2b516a
limyt0 <dudx45@gmail.com>
2026-04-03 12:58:03 +0900

    update: MonsterAIController for Behavior tree

--- Source/SagoMagic/Enemy/SMMonsterAIController.cpp
+++ Source/SagoMagic/Enemy/SMMonsterAIController.cpp
@@ -1,7 +1,8 @@
-﻿#include "Enemy/SMMonsterAIController.h"
+#include "Enemy/SMMonsterAIController.h"
 
 ASMMonsterAIController::ASMMonsterAIController()
 {
+    BlackboardComp = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComp"));
 }
 
 void ASMMonsterAIController::OnPossess(APawn* InPawn)
@@ -9,5 +10,14 @@ void ASMMonsterAIController::OnPossess(APawn* InPawn)
     Super::OnPossess(InPawn);
 
     //나중에 behaviorTree
-    MoveToLocation(FVector::ZeroVector);
+    //MoveToLocation(FVector::ZeroVector);
+    // 블랙보드 에셋이 유효한지 확인 후 초기화
+    if (BBAsset)
+    {
+        // 1. 블랙보드 데이터를 초기화하여 사용 준비
+        UseBlackboard(BBAsset, BlackboardComp);
+
+        // 2. 비헤이비어 트리 실행 (내부적으로 블랙보드를 참조함)
+        RunBehaviorTree(BTAsset);
+    }
 }
--- Source/SagoMagic/Enemy/SMMonsterAIController.h
+++ Source/SagoMagic/Enemy/SMMonsterAIController.h
@@ -1,7 +1,9 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
 #include "SMMonsterAIController.generated.h"
 
 UCLASS()
@@ -12,4 +14,17 @@ public:
     ASMMonsterAIController();
 protected:
     virtual void OnPossess(APawn* InPawn) override;
+
+public:
+
+    UPROPERTY(EditAnywhere, Category = "AI")
+    class UBehaviorTree* BTAsset;
+
+    /** 에디터에서 할당할 블랙보드 에셋 **/
+    UPROPERTY(EditAnywhere, Category = "AI")
+    class UBlackboardData* BBAsset;
+
+    /** 실제로 런타임에 데이터를 담을 컴포넌트 **/
+    UPROPERTY()
+    class UBlackboardComponent* BlackboardComp;
 };

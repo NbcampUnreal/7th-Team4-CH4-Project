20e170ef6948fbc2bf44d386138677a0474b55b5
JRoLee <cnsfw123@gmail.com>
2026-04-03 10:23:04 +0900

    add : SMLobbyGameState class
    add : SMLobbyGameState class

---
+++ Source/SagoMagic/Core/SessionSubsystem/SMLobbyGameState.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SMLobbyGameState.h"
+#include "Net/UnrealNetwork.h"
+
+void ASMLobbyGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(ASMLobbyGameState, PlayerSlots);
+}
+
+void ASMLobbyGameState::UpdatePlayerSlots(const TArray<FSMPlayerSlotInfo>& NewSlots)
+{
+    PlayerSlots = NewSlots;
+}
+
+void ASMLobbyGameState::OnRep_PlayerSlots()
+{
+    OnPlayerSlotChanged.Broadcast();
+}

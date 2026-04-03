9fbd66f307f208bf0fe50e16f3b3f444a176fd58
JRoLee <cnsfw123@gmail.com>
2026-04-03 15:38:27 +0900

    add : empty gamemode for L_Main
    add : empty gamemode for L_Main

---
+++ Source/SagoMagic/Core/SessionSubsystem/SMMainGameMode.h
@@ -0,0 +1,16 @@
+﻿// SMMainGameMode.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SMMainGameMode.generated.h"
+
+/**
+ * L_Main에서 사용할 게임모드
+ */
+UCLASS()
+class SAGOMAGIC_API ASMMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};

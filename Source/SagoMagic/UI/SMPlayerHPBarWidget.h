1135b4020d89ad16dbf87fbc3436934470c56de7
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-03 17:18:53 +0900

    add : PlayerHPBar

---
+++ Source/SagoMagic/UI/SMPlayerHPBarWidget.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameplayEffectTypes.h"
+#include "SMPlayerHPBarWidget.generated.h"
+
+
+class UProgressBar;
+class UTextBlock;
+class UAbilitySystemComponent;
+
+UCLASS()
+class SAGOMAGIC_API USMPlayerHPBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	virtual void NativeTick(const FGeometry& Geometry, float DeltaTime) override;
+	
+	void UpdateHPText(float CurrentHP, float MaxHP);
+	
+	/** GAS - 체력 변경 시 호출될 콜백 함수 */
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+	void OnMaxHealthChanged(const FOnAttributeChangeData& Data);
+	
+protected:
+	FText HPTextTemplate;
+	
+	UPROPERTY(meta=(BindWidget))
+	UProgressBar* ProgressBar_PlayerHP;
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TextBlock_PlayerHP;
+	
+	float TargetPercent = 1.0f;
+	float CurrentPercent = 1.0f;
+	
+	float CachedMaxHP = 1.0f;
+	float CachedCurrentHP = 1.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "UI Settings")
+	float InterpSpeed = 15.0f;
+	
+private:
+	/** 메모리 누수 및 크래시 방지 */
+	FDelegateHandle HealthChangedHandle;
+	FDelegateHandle MaxHealthChangedHandle;
+	
+	UAbilitySystemComponent* GetPlayerASC() const;
+};

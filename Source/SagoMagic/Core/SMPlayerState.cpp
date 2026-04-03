a6e3dd07dfc83e627c5d47cc372ebffd59caeb9d
JRoLee <cnsfw123@gmail.com>
2026-04-03 15:43:58 +0900

    update : add onlie functions to SMPlayerCotroller and SMPlayerState
    update : add onlie functions to SMPlayerCotroller and SMPlayerState

--- Source/SagoMagic/Core/SMPlayerState.cpp
+++ Source/SagoMagic/Core/SMPlayerState.cpp
@@ -5,6 +5,7 @@
 
 #include "AbilitySystemComponent.h"
 #include "GAS/AttributeSets/SMPlayerAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
 ASMPlayerState::ASMPlayerState()
 {
@@ -21,4 +22,24 @@ ASMPlayerState::ASMPlayerState()
 UAbilitySystemComponent* ASMPlayerState::GetAbilitySystemComponent() const
 {
 	return SMAbilitySystemComponent;
+}
+
+void ASMPlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+	
+	ASMPlayerState* NewPS = Cast<ASMPlayerState>(PlayerState);
+	if (IsValid(NewPS) == false) return;
+	
+	NewPS->bIsHost = bIsHost;
+	NewPS->bIsReady = bIsReady;
+	NewPS->SetPlayerName(GetPlayerName());
+}
+
+void ASMPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ASMPlayerState, bIsHost);
+	DOREPLIFETIME(ASMPlayerState, bIsReady);
 }

ef24e5bae50decb0ddd5135c06f9529a77b3d9f1
JRoLee <cnsfw123@gmail.com>
2026-04-04 18:06:42 +0900

    update : seperate login fuction to SMTitlePlayerContorller
    update : seperate login fuction to SMTitlePlayerContorller

--- Source/SagoMagic/Character/SMPlayerController.h
+++ Source/SagoMagic/Character/SMPlayerController.h
@@ -4,11 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
-#include "Interfaces/OnlineSessionInterface.h"
 #include "SMPlayerController.generated.h"
 
 class USMSessionSubsystem;
-class USMMainWidget;
+class USMTitleWidget;
 class USMLobbyWidget;
 /**
  * UI 조작 및 캐릭터에 빙의 후 조작할 컨트롤러
@@ -26,10 +25,6 @@ protected:
 	// 네트워크 이동 기능
 	//================================
 public:
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-	// USMainWidget에서 호출
-	void SetPendingServerAddress(const FString& Address);
-	
 	// ASMGameMode의 HandleSeamlessTravelPlayer에서 호출
 	void OnArrivedAtGameLevel();
 	
@@ -43,31 +38,12 @@ public:
 	
 private:
 	UPROPERTY(EditDefaultsOnly,Category = "UI")
-	TSubclassOf<USMMainWidget> MainWidgetClass;
-	
-	UPROPERTY(EditDefaultsOnly,Category = "UI")
 	TSubclassOf<USMLobbyWidget> LobbyWidgetClass;
+	UPROPERTY(EditDefaultsOnly,Category = "UI")
+	TObjectPtr<USMLobbyWidget> LobbyWidgetInstance;
 	
-	FString PendingServerAddress = TEXT("127.0.0.1:7777");
-	
-	UPROPERTY(EditDefaultsOnly,Category = "Server|Maps")
-	FString MainMapName = TEXT("L_Main");
 	UPROPERTY(EditDefaultsOnly,Category = "Server|Maps")
 	FString LobbyMapName = TEXT("L_Lobby");
 	
-	UPROPERTY()
-	TObjectPtr<USMSessionSubsystem> SessionSubsystem;
-	
-	void ShowMainWidget();
-	void ShowLobbyWidget();
-	
-	UFUNCTION()
-	void OnCreateSessionComplete(bool bWasSuccessful);
-	void OnJoinSessionComplete(EOnJoinSessionCompleteResult::Type Result);
-	
-	void TravelToServer();
-	
-	void BindSessionDelegates();
-	void UnbindSessionDelegates();
-	
+	void ShowLobbyWidget();	
 };

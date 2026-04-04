15fa96611be4177223bbeababdacc15d12b7f619
Park WonJong <kidsmon@naver.com>
2026-04-04 15:47:34 +0900

    add: implement GA_Interact

--- Source/SagoMagic/GAS/Abilities/GA_Interact.h
+++ Source/SagoMagic/GAS/Abilities/GA_Interact.h
@@ -7,10 +7,22 @@
 #include "GA_Interact.generated.h"
 
 /**
- * 
+ * 플레이어의 DefaultAbility중 하나
+ * 다른 액터와 상호작용 하기 위한 어빌리티
  */
 UCLASS()
 class SAGOMAGIC_API UGA_Interact : public UGameplayAbility
 {
 	GENERATED_BODY()
+	
+public:
+	UGA_Interact();
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
 };

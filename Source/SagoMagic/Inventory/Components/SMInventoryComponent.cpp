dd1c966b78e2287b492e066570d065f47bfba0fd
Cheolmin Joo <cjfals4539@gmail.com>
2026-04-04 22:37:14 +0900

    update: added basic InventoryComponent functions
    인벤토리 관련 기본 로직들 구현

--- Source/SagoMagic/Inventory/Components/SMInventoryComponent.cpp
+++ Source/SagoMagic/Inventory/Components/SMInventoryComponent.cpp
@@ -29,7 +29,15 @@ void USMInventoryComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	/** TODO: 기본 인벤토리 초기화 처리 */
+	if (GetOwner() == nullptr)
+	{
+		return;
+	}
+
+	if (GetOwner()->HasAuthority())
+	{
+		InitializeMainInventory();
+	}
 }
 
 void USMInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -51,7 +59,160 @@ FGuid USMInventoryComponent::AddItemFromDefinition(const TSoftObjectPtr<USMItemD
 
 FGuid USMInventoryComponent::AddItemFromDropPayload(const FSMItemDropPayload& InDropPayload)
 {
-	return FGuid();
+	if (GetOwner() == nullptr)
+	{
+		return FGuid();
+	}
+
+	if (GetOwner()->HasAuthority() == false)
+	{
+		return FGuid();
+	}
+
+	if (InDropPayload.IsValidPayload() == false)
+	{
+		return FGuid();
+	}
+
+	if (MainInventory.IsValidContainer() == false)
+	{
+		InitializeMainInventory();
+	}
+
+	const USMItemDefinition* ItemDefinition = InDropPayload.GetDefinition().LoadSynchronous();
+	if (ItemDefinition == nullptr)
+	{
+		return FGuid();
+	}
+
+	if (InDropPayload.ItemType == ESMItemType::Skill)
+	{
+		const USMInternalInventoryFragment* InternalInventoryFragment =
+			ItemDefinition->FindFragmentByClass<USMInternalInventoryFragment>();
+		if (InternalInventoryFragment == nullptr)
+		{
+			return FGuid();
+		}
+
+		/** 새로운 스킬 엔트리 생성 */
+		FSMSkillItemInstanceData NewSkillEntry;
+		NewSkillEntry.BaseItem.InstanceId = InDropPayload.InstanceId;
+		NewSkillEntry.BaseItem.ItemType = ESMItemType::Skill;
+		NewSkillEntry.BaseItem.Definition = InDropPayload.GetDefinition();
+		NewSkillEntry.BaseItem.ParentContainerId = MainInventory.ContainerId;
+		NewSkillEntry.BaseItem.GridX = 0;
+		NewSkillEntry.BaseItem.GridY = 0;
+		NewSkillEntry.BaseItem.Rotation = InDropPayload.GetRotation();
+		NewSkillEntry.BaseItem.bLocked = false;
+
+		/** 내부 컨테이너 복원 */
+		FGuid InternalContainerId;
+		if (CreateSkillInternalContainer(
+			NewSkillEntry.BaseItem.InstanceId, InternalInventoryFragment->GetInternalMask(),
+			InternalContainerId) == false)
+		{
+			return FGuid();
+		}
+
+		NewSkillEntry.InternalContainerId = InternalContainerId;
+		SkillEntries.Add(NewSkillEntry);
+
+		int32 FoundGridX = 0;
+		int32 FoundGridY = 0;
+
+		/** 빈 위치 검색 */
+		if (FindAvailablePosition(
+			NewSkillEntry.BaseItem.InstanceId, MainInventory.ContainerId, FoundGridX, FoundGridY) == false)
+		{
+			SkillEntries.RemoveAll(
+				[&](const FSMSkillItemInstanceData& Entry)
+				{
+					return Entry.BaseItem.InstanceId == NewSkillEntry.BaseItem.InstanceId;
+				});
+
+			SkillInternalContainers.RemoveAll(
+				[&](const FSMGridContainerState& ContainerState)
+				{
+					return ContainerState.ContainerId == InternalContainerId;
+				});
+
+			return FGuid();
+		}
+
+		FSMSkillItemInstanceData* EditableSkill = FindEditableSkill(NewSkillEntry.BaseItem.InstanceId);
+		if (EditableSkill == nullptr)
+		{
+			SkillEntries.RemoveAll(
+				[&](const FSMSkillItemInstanceData& Entry)
+				{
+					return Entry.BaseItem.InstanceId == NewSkillEntry.BaseItem.InstanceId;
+				});
+
+			SkillInternalContainers.RemoveAll(
+				[&](const FSMGridContainerState& ContainerState)
+				{
+					return ContainerState.ContainerId == InternalContainerId;
+				});
+
+			return FGuid();
+		}
+
+		EditableSkill->BaseItem.GridX = FoundGridX;
+		EditableSkill->BaseItem.GridY = FoundGridY;
+
+		/** 메인 인벤토리에 아이템 추가 처리 후 메세지 발송*/
+		MainInventory.ContainedItemIds.Add(EditableSkill->BaseItem.InstanceId);
+		PublishInventoryUpdatedMessage(MainInventory.ContainerId);
+		return EditableSkill->BaseItem.InstanceId;
+	}
+
+	/** 새로운 아이템 엔트리 생성 */
+	FSMItemInstanceData NewItemEntry;
+	NewItemEntry.InstanceId = InDropPayload.InstanceId;
+	NewItemEntry.ItemType = InDropPayload.ItemType;
+	NewItemEntry.Definition = InDropPayload.GetDefinition();
+	NewItemEntry.ParentContainerId = MainInventory.ContainerId;
+	NewItemEntry.GridX = 0;
+	NewItemEntry.GridY = 0;
+	NewItemEntry.Rotation = InDropPayload.GetRotation();
+	NewItemEntry.bLocked = false;
+
+	ItemEntries.Add(NewItemEntry);
+
+	int32 FoundGridX = 0;
+	int32 FoundGridY = 0;
+
+	/** 빈 위치 검색 */
+	if (FindAvailablePosition(
+		NewItemEntry.InstanceId, MainInventory.ContainerId, FoundGridX, FoundGridY) == false)
+	{
+		ItemEntries.RemoveAll(
+			[&](const FSMItemInstanceData& Entry)
+			{
+				return Entry.InstanceId == NewItemEntry.InstanceId;
+			});
+
+		return FGuid();
+	}
+
+	FSMItemInstanceData* EditableItem = FindEditableItem(NewItemEntry.InstanceId);
+	if (EditableItem == nullptr)
+	{
+		ItemEntries.RemoveAll(
+			[&](const FSMItemInstanceData& Entry)
+			{
+				return Entry.InstanceId == NewItemEntry.InstanceId;
+			});
+
+		return FGuid();
+	}
+
+	EditableItem->GridX = FoundGridX;
+	EditableItem->GridY = FoundGridY;
+
+	MainInventory.ContainedItemIds.Add(NewItemEntry.InstanceId);
+	PublishInventoryUpdatedMessage(MainInventory.ContainerId);
+	return EditableItem->InstanceId;
 }
 
 bool USMInventoryComponent::RemoveItem(const FGuid& InItemInstanceId)
@@ -91,14 +252,86 @@ bool USMInventoryComponent::SetItemRotation(const FGuid& InItemInstanceId, int32
 bool USMInventoryComponent::CanPlaceItem(const FGuid& InItemInstanceId, const FGuid& InTargetContainerId, int32 InGridX,
                                          int32 InGridY) const
 {
-	/** TODO: 마스크/충돌/컨테이너 유효성 검사 */
-	return false;
+	const FSMGridContainerState* TargetContainer = FindContainer(InTargetContainerId);
+	if (TargetContainer == nullptr)
+	{
+		return false;
+	}
+
+	const FSMItemInstanceData* ItemData = FindItem(InItemInstanceId);
+	int32 RotationToUse = 0;
+
+	if (ItemData != nullptr)
+	{
+		RotationToUse = ItemData->Rotation;
+	}
+	else
+	{
+		const FSMSkillItemInstanceData* SkillData = FindSkill(InItemInstanceId);
+		if (SkillData == nullptr)
+		{
+			return false;
+		}
+
+		RotationToUse = SkillData->BaseItem.Rotation;
+	}
+
+	TArray<FIntPoint> OccupiedCells;
+	if (BuildOccupiedCells(InItemInstanceId, InGridX, InGridY, RotationToUse, OccupiedCells) == false)
+	{
+		return false;
+	}
+
+	for (const FIntPoint& Cell : OccupiedCells)
+	{
+		if (Cell.X < 0 || Cell.Y < 0 || Cell.X >= TargetContainer->ValidMask.Width || Cell.Y >= TargetContainer->
+			ValidMask.Height)
+		{
+			return false;
+		}
+
+		const int32 MaskIndex = (Cell.Y * TargetContainer->ValidMask.Width) + Cell.X;
+		if (TargetContainer->ValidMask.BitMask.IsValidIndex(MaskIndex) == false)
+		{
+			return false;
+		}
+
+		if (TargetContainer->ValidMask.BitMask[MaskIndex] != TEXT('1'))
+		{
+			return false;
+		}
+	}
+
+	if (HasPlacementConflict(InItemInstanceId, InTargetContainerId, InGridX, InGridY))
+	{
+		return false;
+	}
+
+	return true;
 }
 
 bool USMInventoryComponent::FindAvailablePosition(const FGuid& InItemInstanceId, const FGuid& InTargetContainerId,
                                                   int32& OutGridX, int32& OutGridY) const
 {
-	/** TODO: 순차 탐색 기반 빈 위치 검색 */
+	const FSMGridContainerState* TargetContainer = FindContainer(InTargetContainerId);
+	if (TargetContainer == nullptr)
+	{
+		return false;
+	}
+
+	for (int32 Y = 0; Y < TargetContainer->ValidMask.Height; ++Y)
+	{
+		for (int32 X = 0; X < TargetContainer->ValidMask.Width; ++X)
+		{
+			if (CanPlaceItem(InItemInstanceId, InTargetContainerId, X, Y))
+			{
+				OutGridX = X;
+				OutGridY = Y;
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 
@@ -155,14 +388,26 @@ bool USMInventoryComponent::RebuildSkillSummary(const FGuid& InSkillInstanceId)
 
 bool USMInventoryComponent::GetItemData(const FGuid& InItemInstanceId, FSMItemInstanceData& OutItemData) const
 {
-	/** TODO: 레지스트리 조회 처리 */
-	return false;
+	const FSMItemInstanceData* ItemData = FindItem(InItemInstanceId);
+	if (ItemData == nullptr)
+	{
+		return false;
+	}
+
+	OutItemData = *ItemData;
+	return true;
 }
 
 bool USMInventoryComponent::GetSkillData(const FGuid& InSkillInstanceId, FSMSkillItemInstanceData& OutSkillData) const
 {
-	/** TODO: 레지스트리 조회 처리 */
-	return false;
+	const FSMSkillItemInstanceData* SkillData = FindSkill(InSkillInstanceId);
+	if (SkillData == nullptr)
+	{
+		return false;
+	}
+
+	OutSkillData = *SkillData;
+	return true;
 }
 
 bool USMInventoryComponent::GetSkillSummary(const FGuid& InSkillInstanceId, FSMCompiledSkillSummary& OutSummary) const
@@ -173,8 +418,14 @@ bool USMInventoryComponent::GetSkillSummary(const FGuid& InSkillInstanceId, FSMC
 
 bool USMInventoryComponent::GetContainerData(const FGuid& InContainerId, FSMGridContainerState& OutContainerData) const
 {
-	/** TODO: 컨테이너 조회 처리 */
-	return false;
+	const FSMGridContainerState* ContainerData = FindContainer(InContainerId);
+	if (ContainerData == nullptr)
+	{
+		return false;
+	}
+
+	OutContainerData = *ContainerData;
+	return true;
 }
 
 void USMInventoryComponent::OnRep_InventoryStateChanged()
@@ -184,69 +435,148 @@ void USMInventoryComponent::OnRep_InventoryStateChanged()
 
 bool USMInventoryComponent::HasItem(const FGuid& InItemInstanceId) const
 {
-	/** TODO: 일반/스킬 레지스트리 포함 여부 검사 */
-	return false;
+	return FindItem(InItemInstanceId) != nullptr || FindSkill(InItemInstanceId) != nullptr;
 }
 
 bool USMInventoryComponent::OwnsContainer(const FGuid& InContainerId) const
 {
-	/** TODO: 메인/스킬 내부 컨테이너 소유 여부 검사 */
-	return false;
+	return FindContainer(InContainerId) != nullptr;
 }
 
 const FSMItemInstanceData* USMInventoryComponent::FindItem(const FGuid& InItemInstanceId) const
 {
-	/** TODO: ItemRegistry 조회 */
+	for (const FSMItemInstanceData& Entry : ItemEntries)
+	{
+		if (Entry.InstanceId == InItemInstanceId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 const FSMSkillItemInstanceData* USMInventoryComponent::FindSkill(const FGuid& InSkillInstanceId) const
 {
-	/** TODO: SkillRegistry 조회 */
+	for (const FSMSkillItemInstanceData& Entry : SkillEntries)
+	{
+		if (Entry.BaseItem.InstanceId == InSkillInstanceId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 const FSMGridContainerState* USMInventoryComponent::FindContainer(const FGuid& InContainerId) const
 {
-	/** TODO: MainInventory / SkillInternalContainers 조회 */
+	if (MainInventory.ContainerId == InContainerId)
+	{
+		return &MainInventory;
+	}
+
+	for (const FSMGridContainerState& Entry : SkillInternalContainers)
+	{
+		if (Entry.ContainerId == InContainerId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 const USMItemDefinition* USMInventoryComponent::ResolveItemDefinition(const FSMItemInstanceData& InItemData) const
 {
-	/** TODO: Definition 로드 처리 */
-	return nullptr;
+	if (InItemData.Definition.IsNull())
+	{
+		return nullptr;
+	}
+
+	return InItemData.Definition.LoadSynchronous();
 }
 
 FSMItemInstanceData* USMInventoryComponent::FindEditableItem(const FGuid& InItemInstanceId)
 {
-	/** TODO: ItemRegistry 조회 */
+	for (FSMItemInstanceData& Entry : ItemEntries)
+	{
+		if (Entry.InstanceId == InItemInstanceId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 FSMSkillItemInstanceData* USMInventoryComponent::FindEditableSkill(const FGuid& InSkillInstanceId)
 {
-	/** TODO: SkillRegistry 조회 */
+	for (FSMSkillItemInstanceData& Entry : SkillEntries)
+	{
+		if (Entry.BaseItem.InstanceId == InSkillInstanceId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 FSMGridContainerState* USMInventoryComponent::FindEditableContainer(const FGuid& InContainerId)
 {
-	/** TODO: MainInventory / SkillInternalContainers 조회 */
+	if (MainInventory.ContainerId == InContainerId)
+	{
+		return &MainInventory;
+	}
+
+	for (FSMGridContainerState& Entry : SkillInternalContainers)
+	{
+		if (Entry.ContainerId == InContainerId)
+		{
+			return &Entry;
+		}
+	}
+
 	return nullptr;
 }
 
 void USMInventoryComponent::InitializeMainInventory()
 {
-	/** TODO: DefaultMainInventoryMask 기반 초기화 */
+	if (MainInventory.ContainerId.IsValid())
+	{
+		return;
+	}
+
+	MainInventory.ContainerId = FGuid::NewGuid();
+	MainInventory.ContainerType = ESMContainerType::MainInventory;
+	MainInventory.ValidMask = DefaultMainInventoryMask;
+	MainInventory.ContainedItemIds.Reset();
 }
 
 bool USMInventoryComponent::CreateSkillInternalContainer(const FGuid& InSkillInstanceId,
                                                          const FSMGridMaskData& InInternalMask,
                                                          FGuid& OutCreatedContainerId)
 {
-	/** TODO: 스킬 내부 컨테이너 생성 처리 */
-	return false;
+	if (InSkillInstanceId.IsValid() == false)
+	{
+		return false;
+	}
+
+	if (InInternalMask.IsValidMaskData() == false)
+	{
+		return false;
+	}
+
+	FSMGridContainerState NewContainerState;
+	NewContainerState.ContainerId = FGuid::NewGuid();
+	NewContainerState.ContainerType = ESMContainerType::SkillInternal;
+	NewContainerState.ValidMask = InInternalMask;
+	NewContainerState.ContainedItemIds.Reset();
+
+	OutCreatedContainerId = NewContainerState.ContainerId;
+	SkillInternalContainers.Add(NewContainerState);
+	return true;
 }
 
 bool USMInventoryComponent::CanApplyGemToSkillByTags(const USMGemModifierFragment* InGemModifierFragment,
@@ -259,14 +589,173 @@ bool USMInventoryComponent::CanApplyGemToSkillByTags(const USMGemModifierFragmen
 bool USMInventoryComponent::BuildOccupiedCells(const FGuid& InItemInstanceId, int32 InGridX, int32 InGridY,
                                                int32 InRotation, TArray<FIntPoint>& OutOccupiedCells) const
 {
-	/** TODO: 비트마스크 기반 점유 셀 계산 */
-	return false;
+	OutOccupiedCells.Reset();
+
+	const FSMItemInstanceData* ItemData = FindItem(InItemInstanceId);
+	const FSMItemInstanceData* BaseItemData = ItemData;
+
+	if (BaseItemData == nullptr)
+	{
+		const FSMSkillItemInstanceData* SkillData = FindSkill(InItemInstanceId);
+		if (SkillData == nullptr)
+		{
+			return false;
+		}
+
+		BaseItemData = &SkillData->BaseItem;
+	}
+
+	const USMItemDefinition* ItemDefinition = ResolveItemDefinition(*BaseItemData);
+	if (ItemDefinition == nullptr)
+	{
+		return false;
+	}
+
+	const USMGridShapeFragment* GridShapeFragment = ItemDefinition->FindFragmentByClass<USMGridShapeFragment>();
+	if (GridShapeFragment == nullptr)
+	{
+		return false;
+	}
+
+	const FSMGridMaskData& ShapeMask = GridShapeFragment->GetShapeMask();
+	if (ShapeMask.IsValidMaskData() == false)
+	{
+		return false;
+	}
+
+	const int32 NormalizedRotation = ((InRotation % 4) + 4) % 4;
+
+	for (int32 Y = 0; Y < ShapeMask.Height; ++Y)
+	{
+		for (int32 X = 0; X < ShapeMask.Width; ++X)
+		{
+			const int32 MaskIndex = (Y * ShapeMask.Width) + X;
+			if (ShapeMask.BitMask.IsValidIndex(MaskIndex) == false)
+			{
+				continue;
+			}
+
+			if (ShapeMask.BitMask[MaskIndex] != TEXT('1'))
+			{
+				continue;
+			}
+
+			int32 RotatedX = X;
+			int32 RotatedY = Y;
+
+			switch (NormalizedRotation)
+			{
+			case 0:
+				RotatedX = X;
+				RotatedY = Y;
+				break;
+
+			case 1:
+				RotatedX = ShapeMask.Height - 1 - Y;
+				RotatedY = X;
+				break;
+
+			case 2:
+				RotatedX = ShapeMask.Width - 1 - X;
+				RotatedY = ShapeMask.Height - 1 - Y;
+				break;
+
+			case 3:
+				RotatedX = Y;
+				RotatedY = ShapeMask.Width - 1 - X;
+				break;
+			}
+
+			OutOccupiedCells.Add(FIntPoint(InGridX + RotatedX, InGridY + RotatedY));
+		}
+	}
+
+	return OutOccupiedCells.Num() > 0;
 }
 
 bool USMInventoryComponent::HasPlacementConflict(const FGuid& InItemInstanceId, const FGuid& InTargetContainerId,
                                                  int32 InGridX, int32 InGridY) const
 {
-	/** TODO: 기존 배치 아이템과 충돌 검사 */
+	TArray<FIntPoint> TargetCells;
+
+	const FSMItemInstanceData* ItemData = FindItem(InItemInstanceId);
+	int32 RotationToUse = 0;
+
+	if (ItemData != nullptr)
+	{
+		RotationToUse = ItemData->Rotation;
+	}
+	else
+	{
+		const FSMSkillItemInstanceData* SkillData = FindSkill(InItemInstanceId);
+		if (SkillData == nullptr)
+		{
+			return true;
+		}
+
+		RotationToUse = SkillData->BaseItem.Rotation;
+	}
+
+	if (BuildOccupiedCells(InItemInstanceId, InGridX, InGridY, RotationToUse, TargetCells) == false)
+	{
+		return true;
+	}
+
+	for (const FSMItemInstanceData& Entry : ItemEntries)
+	{
+		if (Entry.InstanceId == InItemInstanceId)
+		{
+			continue;
+		}
+
+		if (Entry.ParentContainerId != InTargetContainerId)
+		{
+			continue;
+		}
+
+		TArray<FIntPoint> ExistingCells;
+		if (BuildOccupiedCells(Entry.InstanceId, Entry.GridX, Entry.GridY, Entry.Rotation, ExistingCells) == false)
+		{
+			continue;
+		}
+
+		for (const FIntPoint& TargetCell : TargetCells)
+		{
+			if (ExistingCells.Contains(TargetCell))
+			{
+				return true;
+			}
+		}
+	}
+
+	for (const FSMSkillItemInstanceData& Entry : SkillEntries)
+	{
+		if (Entry.BaseItem.InstanceId == InItemInstanceId)
+		{
+			continue;
+		}
+
+		if (Entry.BaseItem.ParentContainerId != InTargetContainerId)
+		{
+			continue;
+		}
+
+		TArray<FIntPoint> ExistingCells;
+		if (BuildOccupiedCells(Entry.BaseItem.InstanceId, Entry.BaseItem.GridX, Entry.BaseItem.GridY,
+		                       Entry.BaseItem.Rotation, ExistingCells) == false)
+		{
+			continue;
+		}
+
+		for (const FIntPoint& TargetCell : TargetCells)
+		{
+			if (ExistingCells.Contains(TargetCell))
+			{
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 

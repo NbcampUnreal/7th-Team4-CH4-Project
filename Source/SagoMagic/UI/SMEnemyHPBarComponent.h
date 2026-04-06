fdf6828a59c8b3e88916c17ff9786b1c204bcdef
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-06 12:26:58 +0900

    fix : EnemyHP Delegate Code

--- Source/SagoMagic/UI/SMEnemyHPBarComponent.h
+++ Source/SagoMagic/UI/SMEnemyHPBarComponent.h
@@ -8,32 +8,33 @@
 
 class UAbilitySystemComponent;
 
-UCLASS()
+UCLASS(meta=(BlueprintSpawnableComponent))
 class SAGOMAGIC_API USMEnemyHPBarComponent : public UWidgetComponent
 {
     GENERATED_BODY()
 
 public:
     USMEnemyHPBarComponent();
+    
+    /** 몬스터가 ASC 초기화를 완료한 후 호출할 함수 */
+    void InitializeHPBar(UAbilitySystemComponent* InASC);
 
 protected:
     virtual void BeginPlay() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-        FActorComponentTickFunction* ThisTickFunction) override;
 
     /** GAS - 체력 변경 시 호출될 콜백 함수 */
     void OnHPChanged(const FOnAttributeChangeData& Data);
-
-private:
     /** 일정 시간 후 UI 숨기는 함수 */
     void HideHPBar();
-    FTimerHandle HideTimerHandle;
-
-    UPROPERTY(EditAnywhere, Category = "UI", meta = (AllowPrivateAccess = "true"))
-    float DisplayDuration = 2.0f;
-
+    
+private:
     /** 캐싱해둘 ASC 포인터 */
     UPROPERTY()
     TObjectPtr<UAbilitySystemComponent> ASC;
+    
+    FTimerHandle HideTimerHandle;
+
+    UPROPERTY(EditAnywhere, Category = "UI")
+    float DisplayDuration = 2.0f;
 };

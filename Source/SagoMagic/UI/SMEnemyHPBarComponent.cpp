6b3c0a81dde42ca414f7929433ad0241229eff17
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-06 14:40:24 +0900

    fix : refactoring

--- Source/SagoMagic/UI/SMEnemyHPBarComponent.cpp
+++ Source/SagoMagic/UI/SMEnemyHPBarComponent.cpp
@@ -32,6 +32,19 @@ void USMEnemyHPBarComponent::InitializeHPBar(UAbilitySystemComponent* InASC)
         // ASC 체력 델리게이트 바인딩
         ASC->GetGameplayAttributeValueChangeDelegate(USMMonsterAttributeSet::GetHealthAttribute())
         .AddUObject(this, &USMEnemyHPBarComponent::OnHPChanged);
+        
+        // 초기 HP % 세팅 로직, 내부 데이터는 1.0 상태로 초기화
+        USMEnemyHPBarWidget* HPWidget = Cast<USMEnemyHPBarWidget>(GetUserWidgetObject());
+        if (HPWidget)
+        {
+            float CurrentHP = ASC->GetNumericAttribute(USMMonsterAttributeSet::GetHealthAttribute());
+            float MaxHP = ASC->GetNumericAttribute(USMMonsterAttributeSet::GetMaxHealthAttribute());
+            
+            // MaxHP가 0인 경우 대비 방어 코드
+            float HPPercent = (MaxHP > 0.0f) ? (CurrentHP / MaxHP) : 1.0f;
+            
+            HPWidget->UpdateHPBar(HPPercent);
+        }
     }
 }
 
@@ -58,7 +71,7 @@ void USMEnemyHPBarComponent::OnHPChanged(const FOnAttributeChangeData& Data)
 
 void USMEnemyHPBarComponent::HideHPBar()
 {
-    if (IsValid(this) && IsValid(GetOwner()))
+    if (IsValid(this) && IsValid(GetOwner()) && GetWorld())
     {
         SetVisibility(false);
     }

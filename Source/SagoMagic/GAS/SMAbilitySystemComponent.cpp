e93930d3fc23b93f60503d5a44d02022e556d97c
허태린 <ouranos0902@gmail.com>
2026-04-08 10:50:11 +0900

    fix: SMASC Tick delete
    ASC에서 tick 제거

--- Source/SagoMagic/GAS/SMAbilitySystemComponent.cpp
+++ Source/SagoMagic/GAS/SMAbilitySystemComponent.cpp
@@ -2,18 +2,11 @@
 
 USMAbilitySystemComponent::USMAbilitySystemComponent()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = false;
 }
 
 void USMAbilitySystemComponent::BeginPlay()
 {
     Super::BeginPlay();
-}
-
-void USMAbilitySystemComponent::TickComponent(float DeltaTime,
-                                              ELevelTick TickType,
-                                              FActorComponentTickFunction* ThisTickFunction)
-{
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 

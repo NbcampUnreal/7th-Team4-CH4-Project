e3f0b2261c98fa68f9321502ff1170c2cba0d37d
limyt0 <dudx45@gmail.com>
2026-04-09 21:04:54 +0900

    add: SMMonsterAnimInstance

---
+++ Source/SagoMagic/Enemy/SMMonsterAnimInstance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SMMonsterAnimInstance.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API USMMonsterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};

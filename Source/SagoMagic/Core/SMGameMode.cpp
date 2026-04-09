bd047edb1fd25ab5b14dda3150e51f80da95e2f2
Park WonJong <kidsmon@naver.com>
2026-04-09 12:00:25 +0900

    fix: Fixed a bug where players were not spawning

--- Source/SagoMagic/Core/SMGameMode.cpp
+++ Source/SagoMagic/Core/SMGameMode.cpp
@@ -12,97 +12,139 @@ ASMGameMode::ASMGameMode()
 
 void ASMGameMode::HandleSeamlessTravelPlayer(AController*& C)
 {
-    Super::HandleSeamlessTravelPlayer(C);
-    ASMPlayerController* PC = Cast<ASMPlayerController>(C);
-    if (IsValid(PC))
-    {
-        AllPlayerController.Add(PC);
-        PC->ClientRPCArrivedAtGameLevel();
-        TryStartGame();
-    }
+	Super::HandleSeamlessTravelPlayer(C);
+	ASMPlayerController* PC = Cast<ASMPlayerController>(C);
+	if (IsValid(PC))
+	{
+		AllPlayerController.Add(PC);
+		PC->ClientRPCArrivedAtGameLevel();
+		TryStartGame();
+	}
 }
 
 
 void ASMGameMode::Logout(AController* Exiting)
 {
-    Super::Logout(Exiting);
-    ASMPlayerController* PC = Cast<ASMPlayerController>(Exiting);
-    if (IsValid(PC))
-    {
-        AllPlayerController.Remove(PC);
-    }
+	Super::Logout(Exiting);
+	ASMPlayerController* PC = Cast<ASMPlayerController>(Exiting);
+	if (IsValid(PC))
+	{
+		AllPlayerController.Remove(PC);
+
+		// 플레이어가 종료시 타이머 제거
+		if (FTimerHandle* HandlePtr = SpectatorTimerMap.Find(PC))
+		{
+			GetWorldTimerManager().ClearTimer(*HandlePtr);
+		}
+		if (FTimerHandle* HandlePtr = RespawnTimerMap.Find(PC))
+		{
+			GetWorldTimerManager().ClearTimer(*HandlePtr);
+		}
+
+		SpectatorTimerMap.Remove(PC);
+		RespawnTimerMap.Remove(PC);
+	}
 }
 
 void ASMGameMode::BeginPlay()
 {
-    Super::BeginPlay();
-    
-    StateMachine = NewObject<USMStateMachine>(this);
+	Super::BeginPlay();
 
+	StateMachine = NewObject<USMStateMachine>(this);
 }
 
 void ASMGameMode::Tick(float DeltaSeconds)
 {
-    Super::Tick(DeltaSeconds);
-    if (StateMachine)
-        StateMachine->Tick(DeltaSeconds);
+	Super::Tick(DeltaSeconds);
+	if (StateMachine)
+		StateMachine->Tick(DeltaSeconds);
 }
 
 void ASMGameMode::OnPlayerDead(ASMPlayerController* InPlayerController)
 {
 	if (!InPlayerController) return;
-	
-	SM_LOG(this, LogSM, Error, TEXT("[GameMode] 플레이어 사망. 관전모드 및 부활 타이머 시작"));
-	
+
+	SM_LOG(this, LogSM, Error,
+		TEXT("[GameMode] 플레이어 사망. 관전모드(%.1f) 및 부활(%.1f) 타이머 시작"), SpectatorTime, RespawnTime);
+
 	TWeakObjectPtr<ASMPlayerController> WeakPC = InPlayerController;
-	
-	FTimerHandle SpectatorTimerHandle;
-	FTimerDelegate SpectatorDelegate = 
+
+	// 중복 타이머가 있다면 제거
+	if (FTimerHandle* HandlePtr = SpectatorTimerMap.Find(InPlayerController))
+	{
+		GetWorldTimerManager().ClearTimer(*HandlePtr);
+	}
+	if (FTimerHandle* HandlePtr = RespawnTimerMap.Find(InPlayerController))
+	{
+		GetWorldTimerManager().ClearTimer(*HandlePtr);
+	}
+
+	FTimerDelegate SpectatorDelegate =
 		FTimerDelegate::CreateUObject(this, &ThisClass::EnterSpectatorMode, WeakPC);
-	GetWorldTimerManager().SetTimer(SpectatorTimerHandle, SpectatorDelegate, SpectatorTime, false);
-	
-	FTimerHandle RespawnTimerHandle;
+	GetWorldTimerManager().SetTimer(
+		SpectatorTimerMap.FindOrAdd(InPlayerController),
+		SpectatorDelegate,
+		SpectatorTime,
+		false);
+
 	FTimerDelegate RespawnDelegate = FTimerDelegate::CreateUObject(this, &ThisClass::RespawnPlayer, WeakPC);
-	GetWorldTimerManager().SetTimer(RespawnTimerHandle, RespawnDelegate, RespawnTime, false);
+	GetWorldTimerManager().SetTimer(
+		RespawnTimerMap.FindOrAdd(InPlayerController),
+		RespawnDelegate,
+		RespawnTime,
+		false);
 }
 
 void ASMGameMode::EnterSpectatorMode(TWeakObjectPtr<ASMPlayerController> InPlayerController)
 {
 	if (!InPlayerController.IsValid()) return;
-	
+
 	InPlayerController->UnPossess();
-	
+
 	// TODO: 추후 기획 확정 시 관전 처리 로직 작성
 	// 임시로 비행 관전모드 진입
 	InPlayerController->ChangeState(NAME_Spectating);
-	
-	SM_LOG(this, LogSM, Error, TEXT("[GameMode] %f초 뒤 관전 모드 진입"), SpectatorTime);
+
+	SM_LOG(this, LogSM, Error, TEXT("[GameMode] %.1f초 뒤 관전 모드 진입"), SpectatorTime);
 }
 
 void ASMGameMode::RespawnPlayer(TWeakObjectPtr<ASMPlayerController> InPlayerController)
 {
 	if (!InPlayerController.IsValid()) return;
-	
+
 	ASMPlayerController* PC = InPlayerController.Get();
 	
+	// 관전 폰 제거
+	if (APawn* CurrentPawn = PC->GetPawn())
+	{
+		CurrentPawn->Destroy();
+	}
+	PC->UnPossess();
+	PC->ChangeState(NAME_Playing);
+
 	if (ASMPlayerState* PS = InPlayerController->GetPlayerState<ASMPlayerState>())
 	{
 		PS->ResetForRespawn();
 	}
-	
+
 	// 스폰 포인트 지정 후 리스폰
 	AActor* SpawnPoint = ChoosePlayerStart(PC);
 	RestartPlayerAtPlayerStart(PC, SpawnPoint);
-	
+
 	InPlayerController->ClientRPC_HideDeathUI();
 	
-	SM_LOG(this, LogSM, Error, TEXT("[GameMode] %f초 뒤 부활"), RespawnTime);
+	// 부활시 타이머 정리
+	SpectatorTimerMap.Remove(PC);
+	RespawnTimerMap.Remove(PC);
+
+	SM_LOG(this, LogSM, Error, TEXT("[GameMode] %s 플레이어 부활"), *PC->GetName());
 }
+
 void ASMGameMode::TryStartGame()
 {
 	if (AllPlayerController.Num() >= MaxPlayers)
 	{
-		UE_LOG(LogTemp,Warning, TEXT("[GameMode] 모든 플레이어 도착 %d - 게임 시작"),MaxPlayers);
+		UE_LOG(LogTemp, Warning, TEXT("[GameMode] 모든 플레이어 도착 %d - 게임 시작"), MaxPlayers);
 		StateMachine->Initialize(this);
 	}
 }
--- Source/SagoMagic/Core/SMGameMode.h
+++ Source/SagoMagic/Core/SMGameMode.h
@@ -13,49 +13,56 @@ class USMStateMachine;
 UCLASS()
 class SAGOMAGIC_API ASMGameMode : public AGameMode
 {
-    GENERATED_BODY()
-public:
-    ASMGameMode();
-    /** 씸리스 트래블로 입장한 플레이어를 ALlPlayerController에 등록 */
-    virtual void HandleSeamlessTravelPlayer(AController*& C) override;
-    /** 퇴장한 플레이어를 AllPlayerController에서 제거 */
-    virtual void Logout(AController* Exiting) override;
+	GENERATED_BODY()
 
-    /** 게임 시작 진입점 */
-    virtual void BeginPlay() override;
-    /** 매 프레임 StateMachine에 Tick 위임  */
-    virtual void Tick(float DeltaSeconds) override;
-	
+public:
+	ASMGameMode();
+	/** 씸리스 트래블로 입장한 플레이어를 ALlPlayerController에 등록 */
+	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
+	/** 퇴장한 플레이어를 AllPlayerController에서 제거 */
+	virtual void Logout(AController* Exiting) override;
+
+	/** 게임 시작 진입점 */
+	virtual void BeginPlay() override;
+	/** 매 프레임 StateMachine에 Tick 위임  */
+	virtual void Tick(float DeltaSeconds) override;
+
 	/** 플레이어 사망시 호출되는 함수 */
 	void OnPlayerDead(ASMPlayerController* InPlayerController);
 
-    FSimpleDelegate OnWaveCleared;
+	FSimpleDelegate OnWaveCleared;
+
 private:
 	// 관전모드 진입
 	void EnterSpectatorMode(TWeakObjectPtr<ASMPlayerController> InPlayerController);
-	
+
 	// 리스폰
 	void RespawnPlayer(TWeakObjectPtr<ASMPlayerController> InPlayerController);
-	
+
 	/** MaxPlayer만큼 player가 모이면 게임 스타트*/
 	void TryStartGame();
+
 protected:
-    /** 로그인 한 플레이어 Controller 모음 */
-    UPROPERTY()
-    TArray<TObjectPtr<ASMPlayerController>> AllPlayerController;
-	
+	/** 로그인 한 플레이어 Controller 모음 */
+	UPROPERTY()
+	TArray<TObjectPtr<ASMPlayerController>> AllPlayerController;
+
 	/** 관전 모드 진입 시간 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player|Timer")
 	float SpectatorTime = 3.0f;
-	
+
 	/** 부활 시간 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player|Timer")
 	float RespawnTime = 10.0f;
-	
+
 private:
-    /** 게임 플로우를 담당하는 FSM - 서버에만 존재 */
-    UPROPERTY()
-    TObjectPtr<USMStateMachine> StateMachine;
-    
-    int32 MaxPlayers = 2;
+	/** 게임 플로우를 담당하는 FSM - 서버에만 존재 */
+	UPROPERTY()
+	TObjectPtr<USMStateMachine> StateMachine;
+
+	int32 MaxPlayers = 2;
+
+	// 관전 모드 대기 시간과 리스폰 타이머를 플레이어 별로 저장하기 위한 Map
+	TMap<TObjectPtr<ASMPlayerController>, FTimerHandle> SpectatorTimerMap;
+	TMap<TObjectPtr<ASMPlayerController>, FTimerHandle> RespawnTimerMap;
 };

a7953a5fb1e0d8a92f80ed7389c775ea8428adea
limyt0 <dudx45@gmail.com>
2026-04-10 14:27:37 +0900

    update: Modified from inserting the tag every tick to inserting it only once

--- Source/SagoMagic/Enemy/SMMonsterAnimInstance.cpp
+++ Source/SagoMagic/Enemy/SMMonsterAnimInstance.cpp
@@ -1,8 +1,18 @@
 #include "Enemy/SMMonsterAnimInstance.h"
 #include "AbilitySystemComponent.h"   
-#include "GameplayTagContainer.h"     
+ 
 #include "SMMonsterBase.h"
 #include "GAS/AttributeSets/SMMonsterAttributeSet.h"
+
+//FGameplayTag USMMonsterAnimInstance::AttackingTag;
+
+void USMMonsterAnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+
+    // 게임 시작 시 한 번만 태그 캐싱
+    AttackingTag = FGameplayTag::RequestGameplayTag(FName("Enemy.Attacking"));
+}
 
 void USMMonsterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
@@ -30,8 +40,9 @@ void USMMonsterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     // 태그 유무로 bIsAttacking 결정
     if (CachedASC)
     {
-        bIsAttacking = CachedASC->HasMatchingGameplayTag(
+        bIsAttacking = CachedASC->HasMatchingGameplayTag(AttackingTag);
+        /*bIsAttacking = CachedASC->HasMatchingGameplayTag(
             FGameplayTag::RequestGameplayTag(FName("Enemy.Attacking"))
-        );
+        );*/
     }
 }
--- Source/SagoMagic/Enemy/SMMonsterAnimInstance.h
+++ Source/SagoMagic/Enemy/SMMonsterAnimInstance.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"    
 #include "SMMonsterAnimInstance.generated.h"
 
 class ASMMonsterBase;
@@ -12,6 +13,8 @@ class SAGOMAGIC_API USMMonsterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 public:
+    virtual void NativeInitializeAnimation() override;
+
     virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
     /** BT/AI에서 이동 중인지 여부 **/
@@ -31,4 +34,7 @@ private:
     /**  ASC 캐싱(매 틱 Cast 방지)**/
     UPROPERTY()
     TObjectPtr<UAbilitySystemComponent> CachedASC;
+
+    // 태그 한 번만 초기화
+    FGameplayTag AttackingTag;
 };

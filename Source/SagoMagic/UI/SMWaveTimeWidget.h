fcf2e08f1bc38c1d551f6f91733368ea9c1f1b46
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-10 17:14:16 +0900

    add : Create WaveTimeWidget

---
+++ Source/SagoMagic/UI/SMWaveTimeWidget.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SMWaveTimeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAGOMAGIC_API USMWaveTimeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};

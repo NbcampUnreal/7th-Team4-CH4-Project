91fd06274a72b2a9cecd5090f66c7b6fb825713c
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-10 17:15:16 +0900

    add : Create BaseCampWidget

---
+++ Source/SagoMagic/UI/SMBaseCampHPBarWidget.cpp
@@ -0,0 +1,46 @@
+﻿#include "SMBaseCampHPBarWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "GameplayTags/UI/SMUITag.h"
+
+void USMBaseCampHPBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// Listener 등록
+	if (UGameplayMessageSubsystem::HasInstance(this))
+	{
+		UGameplayMessageSubsystem& MessageSystem = UGameplayMessageSubsystem::Get(this);
+        
+		BaseCampListenerHandle = MessageSystem.RegisterListener(SMUITag::Event_BaseCamp, this,
+			&ThisClass::OnBaseCampMessageReceived);
+	}
+}
+
+void USMBaseCampHPBarWidget::NativeDestruct()
+{
+	if (BaseCampListenerHandle.IsValid())
+	{
+		BaseCampListenerHandle.Unregister();
+	}
+    
+	Super::NativeDestruct();
+}
+
+void USMBaseCampHPBarWidget::OnBaseCampMessageReceived(FGameplayTag Channel, const FBaseCampMsg& Message)
+{
+	// 방송 수신하면 프로그레스 바 갱신
+	if (ProgressBar_BaseCampHP)
+	{
+		ProgressBar_BaseCampHP->SetPercent(Message.BaseCampHP);
+	}
+
+	if (TextBlock_BaseCampHP)
+	{
+		int32 Current = FMath::Max(0, FMath::CeilToInt(Message.CurrentHP));
+		int32 Max = FMath::Max(1, FMath::CeilToInt(Message.MaxHP));
+		
+		FText HPText = FText::Format(FText::FromString(TEXT("{0} / {1}")), Current, Max);
+		TextBlock_BaseCampHP->SetText(HPText);
+	}
+}
---
+++ Source/SagoMagic/UI/SMBaseCampHPBarWidget.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/SMGameplayMessages.h"
+#include "SMBaseCampHPBarWidget.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+/**
+ * 베이스 캠프 체력 프로그래스바를 띄우는 위젯
+ */
+UCLASS()
+class SAGOMAGIC_API USMBaseCampHPBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_BaseCampHP;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_BaseCampHP;
+	
+private:
+	/** 방송을 수신했을 때 실행될 콜백 함수 */
+	void OnBaseCampMessageReceived(FGameplayTag Channel, const FBaseCampMsg& Message);
+
+	/** Listener 해제를 위한 핸들러 */
+	UPROPERTY()
+	FGameplayMessageListenerHandle BaseCampListenerHandle;
+};

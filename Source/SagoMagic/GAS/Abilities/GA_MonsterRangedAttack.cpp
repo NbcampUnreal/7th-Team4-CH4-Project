79d30d48039d008b9923ce818bfa0cfc15c9206a
허태린 <ouranos0902@gmail.com>
2026-04-13 00:45:23 +0900

    add: new Monster Ranged Attack Ability class

---
+++ Source/SagoMagic/GAS/Abilities/GA_MonsterRangedAttack.cpp
@@ -0,0 +1,161 @@
+#include "GAS/Abilities/GA_MonsterRangedAttack.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Enemy/SMMonsterBase.h"
+#include "Enemy/SMMonsterProjectile.h"
+#include "GAS/AttributeSets/SMMonsterAttributeSet.h"
+
+UGA_MonsterRangedAttack::UGA_MonsterRangedAttack()
+{
+	// 서버에서만 실행되게
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerOnly;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UGA_MonsterRangedAttack::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!AttackMontage)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	// 공격 상태 태그 부착
+	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	{
+		ASC->AddLooseGameplayTags(AttackingTags);
+	}
+
+	// 몽타주 재생
+	UAbilityTask_PlayMontageAndWait* MontageTask =
+		UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this, FName("RangedAttack"), AttackMontage, 1.0f);
+
+	MontageTask->OnCompleted.AddDynamic(this, &UGA_MonsterRangedAttack::OnMontageCompleted);
+	MontageTask->OnCancelled.AddDynamic(this, &UGA_MonsterRangedAttack::OnMontageCancelled);
+	MontageTask->OnInterrupted.AddDynamic(this, &UGA_MonsterRangedAttack::OnMontageCancelled);
+	MontageTask->ReadyForActivation();
+
+	// AnimNotify 이벤트 대기
+	UAbilityTask_WaitGameplayEvent* WaitEventTask =
+		UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, HitEventTag);
+
+	WaitEventTask->EventReceived.AddDynamic(this, &UGA_MonsterRangedAttack::OnHitEventReceived);
+	WaitEventTask->ReadyForActivation();
+}
+
+void UGA_MonsterRangedAttack::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	// 공격 상태 태그 해제
+	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	{
+		ASC->RemoveLooseGameplayTags(AttackingTags);
+	}
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_MonsterRangedAttack::OnHitEventReceived(FGameplayEventData Payload)
+{
+	if (!GetActorInfo().IsNetAuthority())
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+		return;
+	}
+
+	AActor* SourceActor = GetAvatarActorFromActorInfo();
+	UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo();
+
+	if (!SourceActor || !SourceASC || !ProjectileClass || !DamageEffectClass)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+
+	// 블랙보드의 TargetActor를 향한 방향 계산
+	FVector FireDirection = SourceActor->GetActorForwardVector();
+	if (APawn* MonsterPawn = Cast<APawn>(SourceActor))
+	{
+		if (AAIController* AIController = Cast<AAIController>(MonsterPawn->GetController()))
+		{
+			if (UBlackboardComponent* BB = AIController->GetBlackboardComponent())
+			{
+				if (AActor* Target = Cast<AActor>(BB->GetValueAsObject(FName("TargetActor"))))
+				{
+					const FVector ToTarget = Target->GetActorLocation() - SourceActor->GetActorLocation();
+					if (!ToTarget.IsNearlyZero())
+					{
+						FireDirection = ToTarget.GetSafeNormal();
+					}
+				}
+			}
+		}
+	}
+
+	// GE Spec 생성
+	FGameplayEffectContextHandle EffectContext = SourceASC->MakeEffectContext();
+	EffectContext.AddInstigator(SourceActor,
+	                            Cast<APawn>(SourceActor) ? Cast<APawn>(SourceActor)->GetController() : nullptr);
+
+	FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(),
+	                                                                   EffectContext);
+	if (SpecHandle.IsValid())
+	{
+		SpecHandle.Data->SetSetByCallerMagnitude(
+			FGameplayTag::RequestGameplayTag(TEXT("Data.Damage.Amount")), -GetMonsterAttackPower());
+	}
+
+	// 투사체 스폰
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = SourceActor;
+	SpawnParams.Instigator = Cast<APawn>(SourceActor);
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	ASMMonsterProjectile* Projectile = GetWorld()->SpawnActor<ASMMonsterProjectile>(
+		ProjectileClass,
+		SourceActor->GetActorLocation(),
+		FireDirection.Rotation(),
+		SpawnParams);
+
+	if (Projectile)
+	{
+		Projectile->InitProjectile(SpecHandle, ProjectileRange, FireDirection, SourceActor);
+	}
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UGA_MonsterRangedAttack::OnMontageCompleted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UGA_MonsterRangedAttack::OnMontageCancelled()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+float UGA_MonsterRangedAttack::GetMonsterAttackPower() const
+{
+	ASMMonsterBase* Monster = Cast<ASMMonsterBase>(GetAvatarActorFromActorInfo());
+	if (Monster && Monster->MonsterAttributeSet)
+	{
+		return Monster->MonsterAttributeSet->GetAttackPower();
+	}
+	return 10.0f;
+}

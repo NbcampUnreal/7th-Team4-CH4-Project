5e7fe14a41a48b6b7502e8b62df33f88acfe8ca9
Park WonJong <kidsmon@naver.com>
2026-04-14 16:17:44 +0900

    refactor: add animation to SkillField

--- Source/SagoMagic/GAS/Abilities/GA_SkillField.h
+++ Source/SagoMagic/GAS/Abilities/GA_SkillField.h
@@ -15,12 +15,10 @@ public:
 	UGA_SkillField();
 
 protected:
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	virtual void OnSkillEffect(
 		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		const FGameplayEventData* TriggerEventData) override;
-
-	virtual void OnSkillEffect(const FGameplayAbilityActorInfo* ActorInfo) override;
+		const FVector& TargetLocation,
+		const FVector& AimDirection) override;
 
 	/** 스폰할 장판 액터 클래스 - BP에서 지정 */
 	UPROPERTY(EditDefaultsOnly, Category = "Skill")
@@ -29,14 +27,4 @@ protected:
 	/** 장판 지속 시간 */
 	UPROPERTY(EditDefaultsOnly, Category = "Skill")
 	float FieldDuration = 5.f;
-
-private:
-	// 서버에서 클라이언트 타겟데이터 수신 시 호출
-	void OnTargetDataReady(const FGameplayAbilityTargetDataHandle& TargetData, FGameplayTag ApplicationTag);
-
-	// 장판 스폰 및 초기화
-	void SpawnFieldAtLocation(const FVector& SpawnLocation, const FGameplayAbilityActorInfo* ActorInfo);
-
-	// 마우스 위치
-	bool TryGetMouseGroundLocation(APawn* Pawn, FVector& OutLocation) const;
 };

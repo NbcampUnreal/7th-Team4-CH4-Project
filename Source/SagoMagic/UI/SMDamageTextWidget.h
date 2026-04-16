fee06cbb2842620a8c636d9a000fece11ab56219
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-16 19:02:02 +0900

    add : DamageFloatingText WBP

---
+++ Source/SagoMagic/UI/SMDamageTextWidget.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SMDamageTextWidget.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class SAGOMAGIC_API USMDamageTextWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	/** 데미지 -> 텍스트 업데이트 함수 */
+	void SetDamageText(float DamageAmount);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Damage;
+};

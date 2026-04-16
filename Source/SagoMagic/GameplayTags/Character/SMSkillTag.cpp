fdf58ec20ddb5f6aa3f9278e9a5ec149758a97d5
허태린 <ouranos0902@gmail.com>
2026-04-16 21:19:26 +0900

    fis: skilltag fix
    리베이스 잘못해서 다시 추가

--- Source/SagoMagic/GameplayTags/Character/SMSkillTag.cpp
+++ Source/SagoMagic/GameplayTags/Character/SMSkillTag.cpp
@@ -25,8 +25,8 @@ namespace SMSkillTag
 	//스킬 업그레이드 태그들
 	UE_DEFINE_GAMEPLAY_TAG(Upgrade_Projectile_Multishot, TEXT("Upgrade.Projectile.Multishot"))
 	UE_DEFINE_GAMEPLAY_TAG(Upgrade_Projectile_Homing, TEXT("Upgrade.Projectile.Homing"))
-	
-	// 장판 업그레이드
+	UE_DEFINE_GAMEPLAY_TAG(Upgrade_LineTrace_Penetrate, TEXT("Upgrade.LineTrace.Penetrate"))
+	UE_DEFINE_GAMEPLAY_TAG(Upgrade_LineTrace_Chain, TEXT("Upgrade.LineTrace.Chain"))
 	UE_DEFINE_GAMEPLAY_TAG(Upgrade_Field_Pull, TEXT("Upgrade.Field.Pull"))
 	UE_DEFINE_GAMEPLAY_TAG(Upgrade_Field_Slow, TEXT("Upgrade.Field.Slow"))
 	
@@ -39,5 +39,6 @@ namespace SMSkillTag
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Skill_SpawnField_Tick, TEXT("GameplayCue.Skill.SpawnField_Tick"))
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Skill_LineTrace_Hit, TEXT("GameplayCue.Skill.LineTrace.Hit"))
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Skill_LineTrace_Beam, TEXT("GameplayCue.Skill.LineTrace.Beam"))
+	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Skill_LineTrace_Chain, TEXT("GameplayCue.Skill.LineTrace.Chain"))
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Skill_ApplyInstantDamage_Hit, TEXT("GameplayCue.Skill.ApplyInstantDamage_Hit"))
 }

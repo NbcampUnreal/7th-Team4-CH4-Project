d40774c72a4b23e4991987fd68ed06df97f39f79
Park WonJong <kidsmon@naver.com>
2026-04-17 11:59:32 +0900

    fix: fix typo

--- Source/SagoMagic/Building/SMBaseCampActor.h
+++ Source/SagoMagic/Building/SMBaseCampActor.h
@@ -39,9 +39,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Interaction")
 	TObjectPtr<USMInteractionTargetComponent> InteractionTargetComponent;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Iteraction|Repair")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Interaction|Repair")
 	float RepairCost = 30.0f;
 	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Iteraction|Repair")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Interaction|Repair")
 	float RepairAmount = 5.0f;
 };

2167d01ebae8044c82f9e54261c991cd2b4ee59f
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-19 02:20:21 +0900

    add : BuildingHPBar Widget Code

---
+++ Source/SagoMagic/UI/SMBuildingHPBarWidget.cpp
@@ -0,0 +1,32 @@
+﻿#include "UI/SMBuildingHPBarWidget.h"
+#include "Components/ProgressBar.h"
+
+void USMBuildingHPBarWidget::SetMaxHP(float InMaxHP)
+{
+	CachedMaxHP = FMath::Max(1.f, InMaxHP);
+}
+
+void USMBuildingHPBarWidget::UpdateHPBar(float CurrentHP, float MaxHP)
+{
+	CachedMaxHP   = FMath::Max(1.f, MaxHP);
+	TargetPercent = FMath::Clamp(CurrentHP / CachedMaxHP, 0.f, 1.f);
+}
+
+void USMBuildingHPBarWidget::NativeTick(const FGeometry& Geometry, float DeltaTime)
+{
+	Super::NativeTick(Geometry, DeltaTime);
+
+	if (!FMath::IsNearlyEqual(CurrentPercent, TargetPercent, 0.001f))
+	{
+		CurrentPercent = FMath::FInterpTo(CurrentPercent, TargetPercent, DeltaTime, InterpSpeed);
+	}
+	else
+	{
+		CurrentPercent = TargetPercent;
+	}
+
+	if (ProgressBar_BuildingHP)
+	{
+		ProgressBar_BuildingHP->SetPercent(CurrentPercent);
+	}
+}
---
+++ Source/SagoMagic/UI/SMBuildingHPBarWidget.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SMBuildingHPBarWidget.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+
+/**
+ * 건물 위에 떠있는 HP 바 위젯
+ */
+UCLASS()
+class SAGOMAGIC_API USMBuildingHPBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	/** HP 수치로 바 갱신 */
+	void UpdateHPBar(float CurrentHP, float MaxHP);
+	/** 최대 체력 초기 설정 */
+	void SetMaxHP(float InMaxHP);
+
+protected:
+	virtual void NativeTick(const FGeometry& Geometry, float DeltaTime) override;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_BuildingHP;
+	
+	UPROPERTY(EditAnywhere, Category = "UI Settings")
+	float InterpSpeed = 10.f;
+
+private:
+	float TargetPercent  = 1.f;
+	float CurrentPercent = 1.f;
+	float CachedMaxHP    = 1.f;
+};

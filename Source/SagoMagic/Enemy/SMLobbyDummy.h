13bd96658da165bac43cfa28cdcfba5e06bdc379
JRoLee <cnsfw123@gmail.com>
2026-04-18 23:24:40 +0900

    add : dummy monster class for Lobby
    add : dummy monster class for Lobby

---
+++ Source/SagoMagic/Enemy/SMLobbyDummy.h
@@ -0,0 +1,21 @@
+﻿// ASMLobbyDummy.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SMMonsterBase.h"
+#include "SMLobbyDummy.generated.h"
+
+UCLASS()
+class SAGOMAGIC_API ASMLobbyDummy : public ASMMonsterBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	ASMLobbyDummy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};

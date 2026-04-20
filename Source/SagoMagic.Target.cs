7e0da794dae7a9afc9186779c926cb1a6898b317
Nichipe-JCM <cjfals4539@gmail.com>
2026-04-20 14:31:04 +0900

    fix: fix for packaging
    패키징 위한 수정

--- Source/SagoMagic.Target.cs
+++ Source/SagoMagic.Target.cs
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class SagoMagicTarget : TargetRules
 {
@@ -11,11 +10,5 @@ public class SagoMagicTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("SagoMagic");
-        RegisterModulesCreatedByRider();
-    }
-
-    private void RegisterModulesCreatedByRider()
-    {
-        ExtraModuleNames.AddRange(new string[] { "GitStatusBranch" });
-    }
+	}
 }

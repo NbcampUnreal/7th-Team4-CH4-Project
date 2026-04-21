1f678c45f19d6db9cad54e62ad9125c103b327e2
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-21 19:13:50 +0900

    fix : color issue fix maybe

--- Source/SagoMagic/UI/SMBuildModeWidget.cpp
+++ Source/SagoMagic/UI/SMBuildModeWidget.cpp
@@ -7,39 +7,41 @@
 void USMBuildModeWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
-
+	
 	UGameplayMessageSubsystem& MsgSys = UGameplayMessageSubsystem::Get(this);
 	BuildModeListenerHandle = MsgSys.RegisterListener<FBuildModeMsg>(
 		SMUITag::Event_BuildMode, this, &ThisClass::OnBuildModeMessage);
-
+	
 	SlotBorders.Empty();
+	
 	if (SlotContainer)
 	{
-		for (int32 i = 0; i < SlotContainer->GetChildrenCount(); ++i)
+		const int32 ChildCount = SlotContainer->GetChildrenCount();
+		for (int32 i = 0; i < ChildCount; ++i)
 		{
 			UWidget* Child = SlotContainer->GetChildAt(i);
-			UE_LOG(LogTemp, Warning, TEXT("[BuildModeUI] %d번 자식 클래스: %s"), i, *Child->GetClass()->GetName());
-
-			// SizeBox인 경우
+			UBorder* FoundBorder = nullptr;
+			
 			if (USizeBox* SizeBox = Cast<USizeBox>(Child))
 			{
 				if (SizeBox->GetChildrenCount() > 0)
 				{
-					UWidget* GrandChild = SizeBox->GetChildAt(0);
-					if (UBorder* Border = Cast<UBorder>(GrandChild))
-					{
-						SlotBorders.Add(Border);
-					}
+					FoundBorder = Cast<UBorder>(SizeBox->GetChildAt(0));
 				}
 			}
-			// 바로 Border가 들어간 경우
 			else if (UBorder* DirectBorder = Cast<UBorder>(Child))
 			{
-				SlotBorders.Add(DirectBorder);
+				FoundBorder = DirectBorder;
+			}
+			
+			if (FoundBorder)
+			{
+				SlotBorders.Add(FoundBorder);
+				
+				FoundBorder->SetBrushColor(DefaultColor); 
 			}
 		}
 	}
-	UE_LOG(LogTemp, Warning, TEXT("[BuildModeUI] 최종 발견한 슬롯(Border) 개수 : %d"), SlotBorders.Num());
 	SetVisibility(ESlateVisibility::Collapsed);
 }
 
@@ -59,28 +61,30 @@ void USMBuildModeWidget::OnBuildModeMessage(FGameplayTag Channel, const FBuildMo
 		SetVisibility(ESlateVisibility::Collapsed);
 		return;
 	}
-
+	
 	SetVisibility(ESlateVisibility::SelfHitTestInvisible);
 	
-	// Q, E로 슬롯 인덱스가 변경될 때마다 하이라이트 함수 호출
 	UpdateSlotHighlight(Message.CurrentSlotIndex);
 }
 
 void USMBuildModeWidget::UpdateSlotHighlight(int32 SelectedIndex)
 {
-	UE_LOG(LogTemp, Error, TEXT("[BuildModeUI] 색상 변경 요청! 들어온 인덱스 : %d"), SelectedIndex);
-	
+	if (SlotBorders.IsEmpty())
+	{
+		return;
+	}
+
 	for (int32 i = 0; i < SlotBorders.Num(); ++i)
 	{
-		if (SlotBorders[i])
+		if (UBorder* Border = SlotBorders[i])
 		{
 			if (i == SelectedIndex)
 			{
-				SlotBorders[i]->SetBrushColor(SelectedColor);
+				Border->SetBrushColor(SelectedColor);
 			}
 			else
 			{
-				SlotBorders[i]->SetBrushColor(DefaultColor);
+				Border->SetBrushColor(DefaultColor);
 			}
 		}
 	}

253192cd2edffcabf4cb2be90050482980502059
Nichipe-JCM <cjfals4539@gmail.com>
2026-04-23 11:12:12 +0900

    fix: fixed inventory pivot bug

--- Source/SagoMagic/UI/Inventory/SMInventoryCellWidget.cpp
+++ Source/SagoMagic/UI/Inventory/SMInventoryCellWidget.cpp
@@ -154,8 +154,8 @@ void USMInventoryCellWidget::NativeOnDragDetected(const FGeometry& InGeometry, c
 		FVector2D PivotCellFraction(0.5f, 0.5f);
 		if (LocalSize.X > 0.0f && LocalSize.Y > 0.0f)
 		{
-			PivotCellFraction.X = FMath::Clamp(LocalMousePosition.X / LocalSize.X, 0.0f, 1.0f);
-			PivotCellFraction.Y = FMath::Clamp(LocalMousePosition.Y / LocalSize.Y, 0.0f, 1.0f);
+			PivotCellFraction.X = FMath::Clamp(LocalMousePosition.X / LocalSize.X, 0.0f, 0.9999f);
+			PivotCellFraction.Y = FMath::Clamp(LocalMousePosition.Y / LocalSize.Y, 0.0f, 0.9999f);
 		}
 
 		OutOperation = OwningGrid->CreateDragDropOperationForItem(
